555591516dad99998a977e77d5365ead2ac5a08e
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2022-03-25 21:00:13 +0100

    STEP_1_CollectReadCounts_DECONA2.py : code review for numba optimizations (mostly doc fixes + uint32)

--- STEP_1_CollectReadCounts_DECONA2.py
+++ STEP_1_CollectReadCounts_DECONA2.py
@@ -12,7 +12,7 @@ import getopt
 import logging
 import os
 import numpy as np # numpy arrays
-import numba as nb # compiled numerical algorithms in Python can approach the speeds of C or FORTRAN.
+import numba # make python faster
 import re
 # nested containment lists, similar to interval trees but faster (https://github.com/biocore-ntnu/ncls)
 from ncls import NCLS
@@ -185,50 +185,49 @@ def createExonDict(exons):
 
 
 #################################################
-# oldCount2CountArray
-# function to increase the execution speed with numba in "nopython" mode.
-# This loop is necessary for the parseCountFile function. 
-# Input:
-#   -old2new is a vector with for same sample : index= old sample header index, 
-#   value = sample user order index (eq sample header index in countsArray) 
-#   -countsArray is an empty int numpy array (dim=NbExons*NbSOIs)
-#   -lineIndex is an int corresponding to the old file line index.
-#   -npInt is an int array containing old fragment counts
-
-# Fill the new count table with the old counts if identical sample.
-@nb.njit
-def oldCount2CountArray(old2new,countsArray,lineIndex,npInt):
-    for oldIndex in nb.prange(len(old2new)):
+# oldCount2CountArray :
+# fill countsArray[exonIndex] with appropriate counts from oldCounts, using old2new to
+# know which old samples (ie columns from old2new) go where in countsArray[exonIndex].
+# This small auxiliary function enables numba optimizations.
+# Args:
+#   -countsArray is an int numpy array, row exonIndex will be filled  (dim=NbExons*NbSOIs)
+#   -exonIndex is the index of the current exon (ie row in countsArray)
+#   -oldCounts contains the old counts for exon exonIndex
+#   -old2new is a vector of size oldCounts, old2new[i] is the column index in countsArray where
+#    counts for old sample i (in oldCounts) must be stored, or -1 if sample i must be discarded
+@numba.njit
+def oldCount2CountArray(countsArray,exonIndex,oldCounts,old2new):
+    for oldIndex in numba.prange(len(old2new)):
             if old2new[oldIndex]!=-1:
-                #oldIndex contains indexes of columns CHR,START,END,EXON_ID, 
-                #npInt doesn't contain these columns indexes shift of -4.
-                countsArray[lineIndex][old2new[oldIndex]] = npInt[oldIndex-4] 
+                countsArray[exonIndex][old2new[oldIndex]] = oldCounts[oldIndex]
+
 #################################################
 # parseCountsFile:
 # Input:
-#   - countsFH is a filehandle open for reading, containing
-#     column titles, as produced by this program
+#   - countsFH is a filehandle open for reading, content is a countsFile 
+#     as produced by this program
 #   - exons holds the exon definitions, padded and sorted, as returned
 #     by processBed
 #   - SOIs is a list of strings: the sample names of interest
-#   - countsArray is an empty int numpy array (dim=NbExons*NbSOIs)
-#   - countsFilled is a boolean numpy array filled by false (1D=len(sampleName))
+#   - countsArray is an all-zeroes int numpy array (dim=NbExons*NbSOIs)
+#   - countsFilled is an all-False boolean numpy array, same size as SOIs
 #
 # -> make sure countsFile was produced with the same BED as exons, else die;
-# -> for any sample present in both countsFile and SOIs, fill the sample's
-#    column in countsArray by copying data from countsFile, and set
+# -> for any sample present in both countsFH and SOIs, fill the sample's
+#    column in countsArray by copying data from countsFH, and set
 #    countsFilled[sample] to True
 def parseCountsFile(countsFH,exons,SOIs,countsArray,countsFilled):
     ######################
     # parse header from (old) countsFile
     oldHeader = countsFH.readline().rstrip().split("\t")
-
+    # ignore exon definition headers "CHR", "START", "END", "EXON_ID"
+    del oldHeader[0:4]
     # fill old2new to identify samples of interest that are already in countsFile:
-    # old2new is a vector, same size as oldHeader, value old2new[old] is:
-    # the index of sample oldHeader[old] in SOIs (if present);
-    # -1 otherwise, ie if oldHeader[old] is one of the exon definition columns "CHR",
-    #    "START", "END", "EXON_ID" or if it's a sample absent from SOIs
-    old2new = np.array([-1]*len(oldHeader))
+    # old2new is a vector, size is the number of samples in countsFH, value old2new[old] is:
+    # the index in SOIs (if present) of the old sample (at index "old" in countsFH, 
+    #   ignoring exon definitions);
+    # -1 otherwise, ie if the old sample at index "old" is absent from SOIs
+    old2new = np.full(len(oldHeader), -1, dtype=int)
     for oldIndex in range(len(oldHeader)):
         for newIndex in range(len(SOIs)):
             if oldHeader[oldIndex] == SOIs[newIndex]:
@@ -237,27 +236,25 @@ def parseCountsFile(countsFH,exons,SOIs,countsArray,countsFilled):
                 break
     ######################
     # parse data lines from countsFile
-    lineIndex = 0
+    exonIndex = 0
     for line in countsFH:
-        splitLine=line.rstrip().split("\t",maxsplit=4) #list of 5 terms. 5th term is equivalent to a str of all counts.
-        # Fill an int numpy array with the old frag counts.
-        # Necessary for the numba use which is restrictive on input variables.
-        npInt=np.fromstring(splitLine[4], dtype=int, sep='\t') 
+        # split into 4 exon definition strings + one string containing all the counts
+        splitLine=line.rstrip().split("\t",maxsplit=4)
         ####### Compare exon definitions
-        if ((splitLine[0]!=exons[lineIndex][0]) or
-            (not splitLine[1].isdigit()) or (int(splitLine[1])!=exons[lineIndex][1]) or
-            (not splitLine[2].isdigit()) or (int(splitLine[2])!=exons[lineIndex][2]) or
-            (splitLine[3]!=exons[lineIndex][3])) :
-            logger.error("exon definitions disagree between previous countsFile %s and the provided BED file "+
-                         "(after padding and sorting) at line index %i. If the BED file changed, a previous "+
-                         "countsFile cannot be re-used: all counts must be recalculated from scratch", lineIndex)
+        if ((splitLine[0]!=exons[exonIndex][0]) or
+            (not splitLine[1].isdigit()) or (int(splitLine[1])!=exons[exonIndex][1]) or
+            (not splitLine[2].isdigit()) or (int(splitLine[2])!=exons[exonIndex][2]) or
+            (splitLine[3]!=exons[exonIndex][3])) :
+            logger.error("exon definitions disagree between previous countsFile and the provided BED file "+
+                         "(after padding and sorting) for exon index %i. If the BED file changed, a previous "+
+                         "countsFile cannot be re-used: all counts must be recalculated from scratch", exonIndex)
             sys.exit(1)
-               
         ###### Fill countsArray with old count data
-        oldCount2CountArray(old2new,countsArray,lineIndex,npInt)#speed up by numba
-        
+        # convert counts from strings to ints, populating a numpy array (needed for numba)
+        oldCounts = np.fromstring(splitLine[4], dtype=np.uint32, sep='\t') 
+        oldCount2CountArray(countsArray,exonIndex,oldCounts,old2new)
         ###### next line
-        lineIndex+=1
+        exonIndex += 1
 
 ####################################################
 # countFrags :
@@ -700,9 +697,8 @@ ARGUMENTS:
 
     # countsArray[exonIndex][sampleIndex] will store the corresponding count.
     # order=F should improve performance, since we fill the array one column at a time.
-    # dtype=uint32 should also be faster and totally sufficient to store the counts,
-    # but in my tests it is MUCH slower than dtype=int !
-    countsArray = np.zeros((len(exons),len(sampleNames)),dtype=int, order='F')
+    # dtype=np.uint32 should also be faster and totally sufficient to store the counts
+    countsArray = np.zeros((len(exons),len(sampleNames)),dtype=np.uint32, order='F')
     # countsFilled: same size and order as sampleNames, value will be set 
     # to True iff counts were filled from countsFile
     countsFilled = np.array([False]*len(sampleNames))
@@ -736,12 +732,12 @@ ARGUMENTS:
     for bamIndex in range(len(bamsToProcess)):
         bam = bamsToProcess[bamIndex]
         sampleName = sampleNames[bamIndex]
-        logger.info('Processing sample %s', sampleName)
         if countsFilled[bamIndex]:
-            logger.info('Sample %s already filled from countsFile, skipping it', sampleName)
+            logger.info('Sample %s already filled from countsFile', sampleName)
             continue
         else:
             try:
+                logger.info('Processing BAM for sample %s', sampleName)
                 countFrags(bam, exonDict, tmpDir, threads, countsArray, bamIndex)
                 thisTime = time.time()
                 logger.debug("Done processing BAM for %s, in %.2f s", sampleName, thisTime-startTime)
