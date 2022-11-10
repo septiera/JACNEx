f1df1300c8a149fdf5fbd490a53a463def008a4c
septiera <amandine.septier@univ-grenoble-alpes.fr>
2022-11-10 11:48:30 +0100

    2_Normalisation_Clustering.py: code homogenisation

--- 2_Normalisation_Clustering.py
+++ 2_Normalisation_Clustering.py
@@ -54,43 +54,43 @@ def extractGonosomesInfos(gender,exons):
     # Takes into account the different non-human organisms 
     # no fixed gender number
     # no fixed chromosomes number
-    genderInfosDict=dict()
+    genderInfosDict = dict()
     for row in gender:
         tmpChrList=[] # will contain all chromosomes specified for the current gender
         for i in range(len(row)):
             # Key definition
-            if i ==0:
-                genderName=row[i]
+            if (i == 0):
+                genderName = row[i]
             # Value definition
             # checking the input syntax format
             # force to include "chr" before the chromosome ID
             # nothing happens if it already has
             else:
-                if not row[i].startswith("chr"):
-                    addChr="chr"+row[i]
+                if (not row[i].startswith("chr")):
+                    addChr = "chr"+row[i]
                     tmpChrList.append(addChr)
                 else:
                     tmpChrList.append(row[i])
             # filling dictionary with corrects infos
-            genderInfosDict[genderName]=tmpChrList
+            genderInfosDict[genderName] = tmpChrList
 
     # sexChrToBedIndexDict building
     # is used to extract the exon indexes specific to each sex chromosome
-    sexChrToBedIndexDict=dict()
+    sexChrToBedIndexDict = dict()
     # list containing only unique chromosome identifiers for all gender
     SexChrFlatList = np.unique([item for sublist in list(genderInfosDict.values()) for item in sublist])
     for exonId in range(len(exons)):
-        if exons[exonId][0] in SexChrFlatList:
-            if exons[exonId][0] in sexChrToBedIndexDict:
+        if (exons[exonId][0] in SexChrFlatList):
+            if (exons[exonId][0] in sexChrToBedIndexDict):
                 sexChrToBedIndexDict[exons[exonId][0]].append(exonId)
             else:
-                sexChrToBedIndexDict[exons[exonId][0]] =[exonId]
+                sexChrToBedIndexDict[exons[exonId][0]] = [exonId]
         else:
             continue
 
     # Checks if the chromosomes are found in the bed file 
     # if not returns an error and break process
-    if len(set(SexChrFlatList)-set(sexChrToBedIndexDict))!=0:
+    if (len(set(SexChrFlatList)-set(sexChrToBedIndexDict))!=0):
         logger.error("Gonosome %s not defined in bedFile.Please check.\n \
             Tip: for all non-numerary gonosomes processBed has annotated them maxCHR+1.(except for X and Y) ",
             set(SexChrFlatList)-set(sexChrToBedIndexDict))
@@ -109,11 +109,11 @@ def extractGonosomesInfos(gender,exons):
 @numba.njit
 def FPMNormalisation(countsArray):
     #create an empty array to filled with the normalized counts
-    countsNorm=np.zeros_like(countsArray,dtype=np.float32)
+    countsNorm = np.zeros_like(countsArray,dtype=np.float32)
     for sampleCol in range(countsArray.shape[1]):
-        SampleCountsSum=np.sum(countsArray[:,sampleCol])
-        SampleCountNorm=(countsArray[:,sampleCol]*1e6)/SampleCountsSum #1e6 is equivalent to 1x10^6
-        countsNorm[:,sampleCol]=SampleCountNorm
+        SampleCountsSum = np.sum(countsArray[:,sampleCol])
+        SampleCountNorm = (countsArray[:,sampleCol]*1e6)/SampleCountsSum #1e6 is equivalent to 1x10^6
+        countsNorm[:,sampleCol] = SampleCountNorm
     return(countsNorm)    
 
 ##############################################
@@ -139,7 +139,7 @@ def clusterBuilding(FPMArray, SOIs, minSampleInCluster, minLinks):
     ##################
     #corrcoef return Pearson product-moment correlation coefficients.
     #rowvar=False parameter allows you to correlate the columns=sample and not the rows[exons].
-    correlation=np.round(np.corrcoef(FPMArray,rowvar=False),2) #correlation value decimal < -10² = tricky to interpret
+    correlation = np.round(np.corrcoef(FPMArray,rowvar=False),2) #correlation value decimal < -10² = tricky to interpret
     ######################################################
     # Distance: 
     ##################
@@ -169,77 +169,77 @@ def clusterBuilding(FPMArray, SOIs, minSampleInCluster, minLinks):
         
     # Accumulators:
     # Attribute cluster identifier [int]
-    clusterCounter=0
+    clusterCounter = 0
     
     # To fill
     # 2D np array with different column typing, dim = NbSOIs*5columns
-    resClustering=np.full((len(SOIs),5),(np.int,"",np.int,"",1))
+    resClustering = np.full((len(SOIs),5),(np.int,"",np.int,"",1))
    
     # Distances range definition 
     # add 0.01 to minLinks as np.arrange(start,stop,step) don't conciderate stop in range 
-    distanceStep=0.05
-    linksRange=np.arange(distanceStep,minLinks+0.01,distanceStep)
+    distanceStep = 0.05
+    linksRange = np.arange(distanceStep,minLinks+0.01,distanceStep)
  
     for selectLinks in linksRange:
         # fcluster : Form flat clusters from the hierarchical clustering defined by 
         # the given linkage matrix.
         # Return An 1D array, dim= sampleIndex[groupNb]
-        groupFormedList=scipy.cluster.hierarchy.fcluster(samplesLinks, selectLinks, criterion='distance')
+        groupFormedList = scipy.cluster.hierarchy.fcluster(samplesLinks, selectLinks, criterion='distance')
 
         # a 1D array with the unique identifiers of the groups
-        uniqueClusterID=np.unique(groupFormedList)
+        uniqueClusterID = np.unique(groupFormedList)
         
         ######################
         # Cluster construction
         # all clusters obtained for a distance value (selectLinks) are processed
         for clusterIndex in range(len(uniqueClusterID)):
             # list of sample indexes associated with this group
-            SOIIndexInCluster,=list(np.where(groupFormedList == uniqueClusterID[clusterIndex]))
+            SOIIndexInCluster, = list(np.where(groupFormedList == uniqueClusterID[clusterIndex]))
             
             #####################
             # Group selection criterion, enough samples number in cluster
-            if len(SOIIndexInCluster)>=minSampleInCluster:
+            if (len(SOIIndexInCluster)>=minSampleInCluster):
                 # New samples to fill in numpy array, it's list of SOIs indexes
-                SOIToAddList=set(SOIIndexInCluster)-set(resClustering[:,0])
+                SOIToAddList = set(SOIIndexInCluster)-set(resClustering[:,0])
                 
                 # New cluster if new samples are presents
                 # For clusters with the same composition from one distance threshold to the other,
                 # no analysis is performed                                       
-                if len(SOIToAddList)!=0:
-                    clusterCounter+=1
+                if (len(SOIToAddList)!=0):
+                    clusterCounter += 1
                     for SOIIndex in SOIIndexInCluster:
-                        if SOIIndex in SOIToAddList:
+                        if (SOIIndex in SOIToAddList):
                             # fill informations for new patient
-                            resClustering[SOIIndex,0]=SOIIndex
-                            resClustering[SOIIndex,1]=SOIs[SOIIndex]
-                            resClustering[SOIIndex,2]=clusterCounter 
+                            resClustering[SOIIndex,0] = SOIIndex
+                            resClustering[SOIIndex,1] = SOIs[SOIIndex]
+                            resClustering[SOIIndex,2] = clusterCounter 
                         else:
                             # update informations for patient used as control
                             # need to pass countClusters in str for the final format 
-                            if resClustering[SOIIndex,3]!="":
-                                ControlGp=resClustering[SOIIndex,3].split(",")
-                                if str(clusterCounter) not in ControlGp:
+                            if (resClustering[SOIIndex,3]!=""):
+                                ControlGp = resClustering[SOIIndex,3].split(",")
+                                if (str(clusterCounter) not in ControlGp):
                                     ControlGp.append(str(clusterCounter))
-                                    resClustering[SOIIndex,3]=",".join(ControlGp)
+                                    resClustering[SOIIndex,3] = ",".join(ControlGp)
                                 else:
                                     continue
                             else:
-                                resClustering[SOIIndex,3]=str(clusterCounter)
+                                resClustering[SOIIndex,3] = str(clusterCounter)
             
             # not enough samples number in cluster              
             else: 
                 #####################
                 # Case where it's the last distance threshold and the samples have never been seen
                 # New cluster creation with dubious samples for calling step 
-                if selectLinks==linksRange[-1]:
+                if (selectLinks==linksRange[-1]):
                     clusterCounter+=1
-                    logger.warning("Creation of cluster n°%s with insufficient numbers %s\
-                    with low correlation %s",clusterCounter,str(len(SOIIndexInCluster)),str(selectLinks))
+                    logger.warning("Creation of cluster n°%s with insufficient numbers %s with low correlation %s",
+                    clusterCounter,str(len(SOIIndexInCluster)),str(selectLinks))
                     for SOIIndex in SOIIndexInCluster:
-                        resClustering[SOIIndex,0]=SOIIndex
-                        resClustering[SOIIndex,1]=SOIs[SOIIndex]
-                        resClustering[SOIIndex,2]=clusterCounter 
-                        resClustering[SOIIndex,4]=0
+                        resClustering[SOIIndex,0] = SOIIndex
+                        resClustering[SOIIndex,1] = SOIs[SOIIndex]
+                        resClustering[SOIIndex,2] = clusterCounter 
+                        resClustering[SOIIndex,4] = 0
                 else:
                     continue  
     return(resClustering,samplesLinks)
@@ -248,26 +248,23 @@ def clusterBuilding(FPMArray, SOIs, minSampleInCluster, minLinks):
 ######################################## Main ##################################################
 ################################################################################################
 def main():
-
-    scriptName=os.path.basename(sys.argv[0])
-
+    scriptName = os.path.basename(sys.argv[0])
     ##########################################
     # parse user-provided arguments
     # mandatory args
-    countsFile=""
+    countsFile = ""
     ##########################################
     # optionnal arguments
     # default values fixed
-    gender=[["male","X","Y"],["female","X","X"]]
-    genderFrom=""
-    minSample=20
-    minLinks=0.25
+    gender = [["male","X","Y"],["female","X","X"]]
+    genderFrom = ""
+    minSample = 20
+    minLinks = 0.25
 
     usage = """\nCOMMAND SUMMARY:
 Given a TSV of exon fragment counts, normalizes the counts (Fragment Per Million) and forms the reference 
 groups for the call. 
 Results are printed to stdout folder : 
-- a TSV file format: first 4 columns hold the exon definitions, subsequent columns hold the normalised counts.
 - a TSV file format: describe the distribution of samples in the reference groups (5 columns);
   first column sample of interest (SOIs), second reference group number for autosomes, third the group to be compared 
   for calling, fourth and fifth identical but for gonosomes.
@@ -291,34 +288,34 @@ ARGUMENTS:
 
     for opt, value in opts:
         # sanity-check and store arguments
-        if opt in ('-h', '--help'):
+        if (opt in ('-h', '--help')):
             sys.stderr.write(usage)
             sys.exit(0)
-        elif opt in ("--counts"):
-            countsFile=value
-            if not os.path.isfile(countsFile):
+        elif (opt in ("--counts")):
+            countsFile = value
+            if (not os.path.isfile(countsFile)):
                 sys.exit("ERROR : countsFile "+countsFile+" doesn't exist. Try "+scriptName+" --help.\n")
-        elif opt in ("--gender"):
-            gender =value
+        elif (opt in ("--gender")):
+            gender = value
             # genotypes is checked later
-        elif opt in ("--gender-from"):
-            genderFrom=value
-            if not os.path.isfile(genderFrom):
+        elif (opt in ("--gender-from")):
+            genderFrom = value
+            if (not os.path.isfile(genderFrom)):
                 sys.exit("ERROR : genders-from file "+genderFrom+" doesn't exist. Try "+scriptName+" --help.\n")
-        elif opt in ("--minSample"):
+        elif (opt in ("--minSample")):
             try:
-                minSample=np.int(value)
+                minSample = np.int(value)
             except Exception as e:
                 logger.error("Conversion of 'minSample' value to int failed : %s", e)
                 sys.exit(1)
-        elif opt in ("--minLinks"):
+        elif (opt in ("--minLinks")):
             try:
-                minLinks=np.float(value)
+                minLinks = np.float(value)
             except Exception as e:
                 logger.error("Conversion of 'minLinks' value to int failed : %s", e)
                 sys.exit(1)
-        elif opt in ("--out"):
-            outFolder=value
+        elif (opt in ("--out")):
+            outFolder = value
             if not os.path.isdir(outFolder):
                 sys.exit("ERROR : outFolder "+outFolder+" doesn't exist. Try "+scriptName+" --help.\n")
         else:
@@ -326,7 +323,7 @@ ARGUMENTS:
 
     #####################################################
     # Check that the mandatory parameters are present
-    if countsFile=="":
+    if countsFile == "":
         sys.exit("ERROR : You must use --counts.\n"+usage)
     
     ######################################################
@@ -341,7 +338,7 @@ ARGUMENTS:
     # - the list of sampleIDs (ie strings) copied from countsFile's header
     # - an int numpy array, dim = len(exons) x len(samples)
     try:
-        exons, SOIs, countsArray= parseCountsFile(countsFile)
+        exons, SOIs, countsArray = parseCountsFile(countsFile)
     except Exception as e:
         logger.error("parseCountsFile failed - %s", e)
         sys.exit(1)
@@ -356,7 +353,7 @@ ARGUMENTS:
     # creation of a list of lists in the same format as "gender" 
     # i.e. [genderName, chr1, chr2, ...].
     if (genderFrom!=""):
-        gender=[]
+        gender = []
         try:
             genderLists = open(genderFrom,"r")
         except Exception as e:
@@ -371,7 +368,7 @@ ARGUMENTS:
     # - genderInfosDict: key: gender name [str], value: chromosomes list [str]
     # - sexChrToBedIndexDict: key: sexChromName [str], value: list of indexes in bedfile [str]
     try :
-        genderInfosDict,sexChrToBedIndexDict=extractGonosomesInfos(gender,exons)
+        genderInfosDict,sexChrToBedIndexDict = extractGonosomesInfos(gender,exons)
     except Exception as e:
         logger.error("extractGonosomesInfos failed - %s", e)
         sys.exit(1)
@@ -384,11 +381,11 @@ ARGUMENTS:
     # Normalisation:
     ##################  
     # allocate countsNorm and populate it with normalised counts of countsArray
-    # same dimension for arrays in input and in output: exonIndex*sampleIndex
+    # same dimension for arrays in input/output: exonIndex*sampleIndex
     # Fragment Per Million normalisation
     # NormCountOneExonForOneSample=(FragsOneExonOneSample*1x10^6)/(TotalFragsAllExonsOneSample)
     try :
-        countsNorm=FPMNormalisation(countsArray)
+        countsNorm = FPMNormalisation(countsArray)
     except Exception as e:
         logger.error("FPMNormalisation failed - %s", e)
         sys.exit(1)
@@ -398,9 +395,9 @@ ARGUMENTS:
 
     # Dicotomisation of data associated with autosomes and gonosomes 
     # Goals: the correlations calculated for clustering are not biased
-    gonoIndex=np.unique([item for sublist in list(sexChrToBedIndexDict.values()) for item in sublist])
-    autosomesFPM=np.delete(countsNorm,gonoIndex,axis=0)
-    gonosomesFPM=np.take(countsNorm,gonoIndex,axis=0)
+    gonoIndex = np.unique([item for sublist in list(sexChrToBedIndexDict.values()) for item in sublist])
+    autosomesFPM = np.delete(countsNorm,gonoIndex,axis=0)
+    gonosomesFPM = np.take(countsNorm,gonoIndex,axis=0)
     
     #####################################################
     # Get Autosomes Clusters
@@ -411,8 +408,9 @@ ARGUMENTS:
     #       3)clusterIDToControl [str], 4) Sample validity for calling [int] 
     # - a 2D numpy array of floats, correspond to the linkage matrix, dim= NbSOIs-1*4. 
     #       Required for the graphical part.
+    logger.info("### Samples clustering on normalised counts of autosomes")
     try :
-        resClusteringAutosomes, sampleLinksAutosomes=clusterBuilding(autosomesFPM, SOIs, minSample, minLinks)
+        resClusteringAutosomes, sampleLinksAutosomes = clusterBuilding(autosomesFPM, SOIs, minSample, minLinks)
     except Exception as e:
         logger.error("clusterBuilding for autosomes failed - %s", e)
         sys.exit(1)
