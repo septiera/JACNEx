c6097dd10a5bfe902880b5240a69831580b88b4e
septiera <amandine.septier@univ-grenoble-alpes.fr>
2022-11-14 19:04:46 +0100

    2_clusterSamps.py: add function genderAttributionPrivate

--- 2_clusterSamps.py
+++ 2_clusterSamps.py
@@ -63,7 +63,7 @@ from mageCNV.normalisation import FPMNormalisation
 # -> 'gendersInfos' is a str list of lists, contains informations for the gender
 # identification, ie ["gender identifier","particular chromosome"].
 # The indexes of the different lists are important:
-# index 0: gender carrying a single gonosome (e.g. human => M:XY)
+# index 0: gender carrying a unique gonosome not present in the other gender (e.g. human => M:XY)
 # index 1: gender carrying two copies of the same gonosome (e.g. human => F:XX) 
 
 def getGenderInfos(exons):
@@ -249,7 +249,7 @@ def clusterBuilds(FPMArray, SOIs, minSampleInCluster, minLinks, figure, outputFi
     # case we wish to obtain the graphical representation
     if figure:
         try:
-            clusterDendograms(resClustering, controlsDict,clusterCounter, samplesLinks, outputFile)
+            clusterDendogramsPrivate(resClustering, controlsDict,clusterCounter, samplesLinks, outputFile)
         except Exception as e: 
             logger.error("clusterDendograms failed - %s", e)
             sys.exit(1)
@@ -267,7 +267,7 @@ def clusterBuilds(FPMArray, SOIs, minSampleInCluster, minLinks, figure, outputFi
 # -outputFile : full path to save the png 
 # Return a png to the user-defined output folder
 
-def clusterDendograms(resClustering, controlsDict,maxClust, sampleLinks, outputFile):
+def clusterDendogramsPrivate(resClustering, controlsDict,maxClust, sampleLinks, outputFile):
     #initialization of a list of strings to complete
     labelsGp=[]
 
@@ -301,6 +301,98 @@ def clusterDendograms(resClustering, controlsDict,maxClust, sampleLinks, outputF
     matplotlib.pyplot.ylabel("Samples of interest")
     matplotlib.pyplot.xlabel("Absolute Pearson distance (1-r)")
     matplotlib.pyplot.savefig(outputFile, dpi=520, format="png", bbox_inches='tight')
+
+######################################
+# Gender and group predicted by Kmeans matching 
+# calcul of normalized count ratios per gonosomes and kmeans group
+# ratio = median (normalized count sums list for a specific gonosome and for all samples in a kmean group)
+# Args:
+#  -kmeans: an int list of groupID predicted by Kmeans ordered on SOIsIndex 
+#  -countsNorm: a float 2D numpy array of normalized count, dim=NbExons*NbSOIs
+#  -gonoIndexDict: a dictionary of correspondence between gonosomeID[key:str] and exonsIndex[value:int list] 
+#  -genderInfoList: a list of list, dim=NbGender*2columns ([genderID, specificChr])
+# Returns a list of genderID where the indices match the groupID formed by the Kmeans.
+# e.g ["F","M"], KmeansGp 0 = Female and KmeansGp 1 = Male
+
+def genderAttributionPrivate(kmeans, countsNorm,gonoIndexDict, genderInfoList):
+    # initiate float variable, 
+    # save the first count ratio for a given gonosome
+    previousCount=None
+
+    # first browse on gonosome names (limited to 2)
+    for gonoID in gonoIndexDict.keys():
+        # int list of the current gonosome exons indexes 
+        gonoExonIndexL=gonoIndexDict[gonoID]
+        
+        # second browse on the kmean groups (only 2)
+        for kmeanGroup in np.unique(kmeans.labels_):
+            # int list corresponding to the sample indexes in the current Kmean group
+            SOIsIndexKGpL,=list(np.where(kmeans.labels_ == kmeanGroup))
+            
+            #####################
+            # selection of specifics normalized count data
+            tmpArray=countsNorm[gonoExonIndexL,] # gonosome exons
+            tmpArray=tmpArray[:,SOIsIndexKGpL] # Kmean group samples        
+            
+            #####################
+            # ratio calcul (axis=0, sum all row/exons for each sample) 
+            countRatio = np.median(np.sum(tmpArray,axis=0))
+
+            # Keep gender names in variables
+            g1 = genderInfoList[0][0] #e.g human g1="M"
+            g2 = genderInfoList[1][0] #e.g human g2="F"
+            
+            #####################
+            # filling two lists corresponding to the gender assignment condition
+            # condition1L and condition2L same construction
+            # 1D string list , dim=2 genderID (indexes corresponds to Kmeans groupID) 
+            if (previousCount!=None):    
+                # row order in genderInfoList is important
+                # 0 : gender with specific gonosome not present in other gender
+                # 1 : gender with 2 copies of same gonosome
+                if gonoID == genderInfoList[0][1]: # e.g human => M:chrY
+                    #####################
+                    # condition assignment gender number 1:
+                    # e.g human case group of M, the chrY ratio is expected 
+                    # to be higher than the group of F (>10*)
+                    countsx10=10*countRatio
+                    condition1L=[""]*2
+                    if previousCount>countsx10:
+                        condition1L[kmeanGroup-1]=g1 #e.g human Kmeans gp0 = M
+                        condition1L[kmeanGroup]=g2 #e.g human Kmeans gp1 = F
+                    else:
+                        condition1L[kmeanGroup-1]=g2 #e.g human Kmeans gp0 = F
+                        condition1L[kmeanGroup]=g1 #e.g human Kmeans gp1 = M
+                else: # e.g human => F:chrX
+                    #####################
+                    # condition assignment gender number 2:
+                    # e.g human case group of F, the chrX ratio should be in 
+                    # the range of 1.5*ratiochrXM to 3*ratiochrXM
+                    countsx1half=3*countRatio/2
+                    condition2L=[""]*2
+                    if previousCount>countsx1half and previousCount<2*countsx1half:
+                        condition2L[kmeanGroup-1]=g2
+                        condition2L[kmeanGroup]=g1
+                    else:
+                        condition2L[kmeanGroup-1]=g1
+                        condition2L[kmeanGroup]=g2
+            
+                # restart for the next gonosome
+                previousCount=None
+            else:
+                # It's the first ratio calculated for the current gonosome => saved for comparison 
+                # with the next ratio calcul
+                previousCount=countRatio
+            
+            
+    # predictions test for both conditions
+    # the two lists do not agree => raise an error and quit the process.
+    if condition1L!=condition1L:
+        logger.error("The conditions of gender allocation are not in agreement.\n \
+            condition n°1, one gender is characterised by a specific gonosome: %s \n \
+                condition n°2 that the other gender is characterised by 2 same gonosome copies: %s ", condition1L, condition2L)
+        sys.exit(1)
+    return(condition1L)
 
 ################################################################################################
 ######################################## Main ##################################################
@@ -485,16 +577,17 @@ ARGUMENTS:
         thisTime = time.time()
         logger.info("Done samples clustering for autosomes : in %.2f s", thisTime-startTime)
         startTime = thisTime
-"""
-    #####################################################
-    # Get Gonosomes Clusters
-    ##################
-    # Different treatment
-    # It is necessary to have the gender genotype information
-    # But without appriori a Kmeans can be made to split the data on gender number
-    logger.info("### Samples clustering on normalised counts of gonosomes")
-    kmeans =sklearn.cluster.KMeans(n_clusters=len(genderInfosDict.keys()), random_state=0).fit(gonosomesFPM.T)#transposition to consider the samples
-"""
+
+        #####################################################
+        # Get Gonosomes Clusters
+        ##################
+        # Different treatment
+        # It is necessary to have the gender genotype information
+        # But without appriori a Kmeans can be made to split the data on gender number
+        logger.info("### Samples clustering on normalised counts of gonosomes")
+        kmeans =sklearn.cluster.KMeans(n_clusters=len(genderInfosDict.keys()), random_state=0).fit(gonosomesFPM.T)#transposition to consider the samples
+
+
 
 if __name__ =='__main__':
     main()
