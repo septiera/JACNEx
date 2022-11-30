aa9cd4f3f7cce594dc1b1237d47403b032a649cf
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2022-11-30 17:38:32 +0100

    mageCNV/countFragments.py : coding style - PEP8 compliance

--- mageCNV/countFragments.py
+++ mageCNV/countFragments.py
@@ -38,7 +38,7 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
     exonNCLs = createExonNCLs(exons)
 
     thisTime = time.time()
-    logger.debug("Done createExonNCLs for %s, in %.2f s",os.path.basename(bamFile), thisTime-startTime)
+    logger.debug("Done createExonNCLs for %s, in %.2f s", os.path.basename(bamFile), thisTime - startTime)
     startTime = thisTime
 
     # We need to process all alignments for a given qname simultaneously
@@ -50,7 +50,7 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
     #   qname (with Qname2ExonCount), then reset accumulators and store
     #   data for new qname.
 
-    # Accumulators: 
+    # Accumulators:
     # QNAME and CHR
     qname, qchrom = "", ""
     # START and END coordinates on the genome of each alignment for this qname,
@@ -61,14 +61,14 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
     qstartOnReadF, qstartOnReadR = [], []
     # qFirstOnForward==1 if the first-in-pair read of this qname is on the
     # forward reference strand, -1 if it's on the reverse strand, 0 if we don't yet know
-    qFirstOnForward=0
+    qFirstOnForward = 0
     # qBad==True if qname must be skipped (e.g. alis on bad or multiple chroms, or alis
     # disagree regarding the strand on which the first/last read-in-pair aligns, or...)
     qBad = False
 
     # To Fill:
     # 1D numpy array containing the sample fragment counts for all exons
-    sampleCounts=np.zeros(len(exons), dtype=np.uint32)
+    sampleCounts = np.zeros(len(exons), dtype=np.uint32)
 
     ############################################
     # Preprocessing:
@@ -100,75 +100,75 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
         if re.match('^@', line):
             continue
 
-        align=line.split('\t', maxsplit=6)
+        align = line.split('\t', maxsplit=6)
 
         ######################################################################
         # If we are done with previous qname: process it and reset accumulators
-        if (qname!=align[0]) and (qname!=""):  # align[0] is the qname
+        if (qname != align[0]) and (qname != ""):  # align[0] is the qname
             if not qBad:
                 # if we have 2 alis on a strand, make sure they are in "read" order (switch them if needed)
-                if (len(qstartF)==2) and (qstartOnReadF[0] > qstartOnReadF[1]):
+                if (len(qstartF) == 2) and (qstartOnReadF[0] > qstartOnReadF[1]):
                     qstartF.reverse()
                     qendF.reverse()
-                if (len(qstartR)==2) and (qstartOnReadR[0] > qstartOnReadR[1]):
+                if (len(qstartR) == 2) and (qstartOnReadR[0] > qstartOnReadR[1]):
                     qstartR.reverse()
                     qendR.reverse()
-                Qname2ExonCount(qstartF,qendF,qstartR,qendR,exonNCLs[qchrom],sampleCounts,maxGap)
+                Qname2ExonCount(qstartF, qendF, qstartR, qendR, exonNCLs[qchrom], sampleCounts, maxGap)
             qname, qchrom = "", ""
             qstartF, qstartR, qendF, qendR = [], [], [], []
             qstartOnReadF, qstartOnReadR = [], []
-            qFirstOnForward=0
-            qBad=False
-        elif qBad: #same qname as previous ali but we know it's bad -> skip
+            qFirstOnForward = 0
+            qBad = False
+        elif qBad:  # same qname as previous ali but we know it's bad -> skip
             continue
 
         ######################################################################
         # Either we're in the same qname and it's not bad, or we changed qname
         # -> in both cases update accumulators with current line
-        if qname=="" :
-            qname=align[0]
+        if qname == "":
+            qname = align[0]
 
         # align[2] == chrom
         if align[2] not in exonNCLs:
             # ignore qname if ali not on a chromosome where we have at least one exon;
             # importantly this skips non-main GRCh38 "chroms" (eg ALT contigs)
-            qBad=True
+            qBad = True
             continue
-        elif qchrom=="":
-            qchrom=align[2]
-        elif qchrom!=align[2]:
+        elif qchrom == "":
+            qchrom = align[2]
+        elif qchrom != align[2]:
             # qname has alignments on different chroms, ignore it
-            qBad=True
+            qBad = True
             continue
         # else same chrom, keep going
 
-        #Retrieving flags for STRAND and first/second read
-        currentFlag=int(align[1])
+        # Retrieving flags for STRAND and first/second read
+        currentFlag = int(align[1])
 
         # currentFirstOnForward==1 if according to this ali, the first-in-pair
         # read is on the forward strand, -1 otherwise
-        currentFirstOnForward=-1
+        currentFirstOnForward = -1
         # flag 16 the alignment is on the reverse strand
         # flag 64 the alignment is first in pair, otherwise 128
-        if ((currentFlag&80) == 64) or ((currentFlag&144) == 144) :
-            currentFirstOnForward=1
-        if qFirstOnForward==0:
+        if ((currentFlag & 80) == 64) or ((currentFlag & 144) == 144):
+            currentFirstOnForward = 1
+        if qFirstOnForward == 0:
             # first ali for this qname
             qFirstOnForward = currentFirstOnForward
-        elif qFirstOnForward != currentFirstOnForward :
-            qBad=True
+        elif qFirstOnForward != currentFirstOnForward:
+            qBad = True
             continue
         # else this ali agrees with previous alis for qname -> NOOP
 
         # START and END coordinates of current alignment on REF (ignoring any clipped bases)
-        currentStart=int(align[3])
+        currentStart = int(align[3])
         # END depends on CIGAR
-        currentCigar=align[5]
-        currentAliLength=aliLengthOnRef(currentCigar)
-        currentEnd=currentStart+currentAliLength-1
+        currentCigar = align[5]
+        currentAliLength = aliLengthOnRef(currentCigar)
+        currentEnd = currentStart + currentAliLength - 1
         # coord of leftmost non-clipped base on read
         currentStartOnRead = firstNonClipped(currentCigar)
-        if (currentFlag&16) == 0:
+        if (currentFlag & 16) == 0:
             # flag 16 off => forward strand
             qstartF.append(currentStart)
             qendF.append(currentEnd)
@@ -182,15 +182,15 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
     # Done reading lines from BAM
 
     # process last Qname
-    if (qname!="") and not qBad:
+    if (qname != "") and not qBad:
         # if we have 2 alis on a strand, make sure they are in "read" order (switch them if needed)
-        if (len(qstartF)==2) and (qstartOnReadF[0] > qstartOnReadF[1]):
+        if (len(qstartF) == 2) and (qstartOnReadF[0] > qstartOnReadF[1]):
             qstartF.reverse()
             qendF.reverse()
-        if (len(qstartR)==2) and (qstartOnReadR[0] > qstartOnReadR[1]):
+        if (len(qstartR) == 2) and (qstartOnReadR[0] > qstartOnReadR[1]):
             qstartR.reverse()
             qendR.reverse()
-        Qname2ExonCount(qstartF,qendF,qstartR,qendR,exonNCLs[qchrom],sampleCounts,maxGap)
+        Qname2ExonCount(qstartF, qendF, qstartR, qendR, exonNCLs[qchrom], sampleCounts, maxGap)
 
     # wait for samtools to finish cleanly and check exit code
     if (samproc.wait() != 0):
@@ -202,7 +202,7 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
     # are in the process of being deleted => sync to avoid race
     os.sync()
     thisTime = time.time()
-    logger.debug("Done countFrags for %s, in %.2f s",os.path.basename(bamFile), thisTime-startTime)
+    logger.debug("Done countFrags for %s, in %.2f s", os.path.basename(bamFile), thisTime - startTime)
     return(sampleCounts)
 
 
@@ -212,17 +212,18 @@ def countFrags(bamFile, exons, maxGap, tmpDir, samtools, samThreads):
 
 ####################################################
 # aliLengthOnRef :
-#Input : a CIGAR string
-#Output : span of the alignment on the reference sequence, ie number of bases
+# Input : a CIGAR string
+# Output : span of the alignment on the reference sequence, ie number of bases
 # consumed by the alignment on the reference
 def aliLengthOnRef(cigar):
-    length=0
-    # only count CIGAR operations that consume the reference sequence, see CIGAR definition 
+    length = 0
+    # only count CIGAR operations that consume the reference sequence, see CIGAR definition
     # in SAM spec available here: https://samtools.github.io/hts-specs/
-    match = re.findall(r"(\d+)[MDN=X]",cigar)
+    match = re.findall(r"(\d+)[MDN=X]", cigar)
     for op in match:
-        length+=int(op)
+        length += int(op)
     return(length)
+
 
 ####################################################
 # firstNonClipped:
@@ -239,6 +240,7 @@ def firstNonClipped(cigar):
         else:
             break
     return(firstNonClipped)
+
 
 #############################################################
 # Create nested containment lists (similar to interval trees but faster), one per
@@ -268,8 +270,9 @@ def createExonNCLs(exons):
     exonNCLs = {}
     for chrom in starts.keys():
         ncl = NCLS(starts[chrom], ends[chrom], indexes[chrom])
-        exonNCLs[chrom]=ncl
+        exonNCLs[chrom] = ncl
     return(exonNCLs)
+
 
 ####################################################
 # Qname2ExonCount :
@@ -285,37 +288,37 @@ def createExonNCLs(exons):
 #   - the maximum accepted gap length between a pair of mate reads, pairs separated by
 #     a longer gap are ignored (putative structural variant or alignment error)
 # Nothing is returned, this function just updates countsVec
-def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,maxGap):
+def Qname2ExonCount(startFList, endFList, startRList, endRList, exonNCL, countsVec, maxGap):
     # skip Qnames that don't have alignments on both strands
-    if (len(startFList)==0) or (len(startRList)==0):
+    if (len(startFList) == 0) or (len(startRList) == 0):
         return
     # skip Qnames that have at least 3 alignments on a strand
-    if (len(startFList)>2) or (len(startRList)>2):
+    if (len(startFList) > 2) or (len(startRList) > 2):
         return
 
     # if we have 2 alis on one strand and they overlap: merge them
     # (there could be a short indel or other SV, not trying to detect
     # this but we still cover this genomic region with this qname)
-    if (len(startFList)==2) and (min(endFList) > max(startFList)):
+    if (len(startFList) == 2) and (min(endFList) > max(startFList)):
         startFList = [min(startFList)]
         endFList = [max(endFList)]
-    if (len(startRList)==2) and (min(endRList) > max(startRList)):
+    if (len(startRList) == 2) and (min(endRList) > max(startRList)):
         startRList = [min(startRList)]
         endRList = [max(endRList)]
-    
+
     #######################################################
     # identify genomic regions covered by the fragment
     #######################################################
     # fragStarts, fragEnds: start and end coordinates of the genomic intervals
     # covered by this fragment.
-    # There is usually one (1F1R overlapping) or 2 intervals (1F1R not 
+    # There is usually one (1F1R overlapping) or 2 intervals (1F1R not
     # overlapping) , but if the fragment spans a DEL there can be up to 3
     fragStarts = []
     fragEnds = []
 
     ##########################
     # 1F1R
-    if (len(startFList)==1) and (len(startRList)==1):
+    if (len(startFList) == 1) and (len(startRList) == 1):
         if (startFList[0] < endRList[0]):
             # face to face
             if (endFList[0] >= startRList[0]):
@@ -337,11 +340,11 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
 
     ##########################
     # 2F1R
-    elif (len(startFList)==2) and (len(startRList)==1):
+    elif (len(startFList) == 2) and (len(startRList) == 1):
         if (startFList[1] <= endRList[0]) and (endFList[1] >= startRList[0]):
             # second F ali overlaps with R ali, build an interval by merging them
-            fragStarts = [min(startFList[1],startRList[0])]
-            fragEnds = [max(endFList[1],endRList[0])]
+            fragStarts = [min(startFList[1], startRList[0])]
+            fragEnds = [max(endFList[1], endRList[0])]
         elif (startFList[1] <= endRList[0]) and (startRList[0] - endFList[1] <= maxGap):
             # second F ali and R ali are face-to-face, they don't overlap but gap
             # is small -> build 2 intervals
@@ -363,7 +366,7 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
                 # Both F alis are overlapped by R ali, doesn't make sense, ignore this qname
                 return
         else:
-            # F alis are inverted, ie beginning of read aligns after end of read on 
+            # F alis are inverted, ie beginning of read aligns after end of read on
             # the genome: spanning a DUP?
             if (fragEnds[-1] < startFList[0]):
                 # indeed this looks like a DUP, with first F ali (==start of F read) an interval on
@@ -375,18 +378,18 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
                 # first F ali overlaps R ali, very small DUP? suspicious, but merge them
                 fragStarts[-1] = min(fragStarts[-1], startFList[0])
                 fragEnds[-1] = max(fragEnds[-1], endFList[0])
-                 # this is a suspected DUP with BPs: fragStarts[0], fragEnds[-1]
+                # this is a suspected DUP with BPs: fragStarts[0], fragEnds[-1]
             else:
                 # F alis suggest a DUP but R ali doesn't agree, ignore this qname
                 return
 
     ##########################
     # 1F2R
-    elif (len(startFList)==1) and (len(startRList)==2):
+    elif (len(startFList) == 1) and (len(startRList) == 2):
         if (startFList[0] <= endRList[0]) and (endFList[0] >= startRList[0]):
             # first R ali overlaps with F ali, build an interval by merging them
-            fragStarts = [min(startFList[0],startRList[0])]
-            fragEnds = [max(endFList[0],endRList[0])]
+            fragStarts = [min(startFList[0], startRList[0])]
+            fragEnds = [max(endFList[0], endRList[0])]
         elif (startFList[0] <= endRList[0]) and (startRList[0] - endFList[0] <= maxGap):
             # first R ali and F ali are face-to-face, they don't overlap but gap
             # is small -> build 2 intervals
@@ -403,7 +406,7 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
                 # indeed this looks like a DEL, with second R ali an interval on its own (on the right)
                 fragStarts.append(startRList[1])
                 fragEnds.append(endRList[1])
-                 # this is a suspected DEL with BPs: fragEnds[-2], fragStarts[-1]
+                # this is a suspected DEL with BPs: fragEnds[-2], fragStarts[-1]
             else:
                 # Both R alis are overlapped by F ali, doesn't make sense, ignore this qname
                 return
@@ -426,14 +429,14 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
 
     ##########################
     # 2F2R
-    elif(len(startFList)==2) and (len(startRList)==2):
+    elif(len(startFList) == 2) and (len(startRList) == 2):
         if (startFList[0] < startFList[1]) and (startRList[0] < startRList[1]):
             # both F and R pairs of alis are in order: spanning a DEL?
             if ((startFList[0] <= endRList[0]) and (endFList[0] >= startRList[0]) and
                 (startFList[1] <= endRList[1]) and (endFList[1] >= startRList[1])):
                 # OK: first F+R reads overlap and same for second pair of F+R reads -> merge each pair
-                fragStarts = [min(startFList[0],startRList[0]), min(startFList[1],startRList[1])]
-                fragEnds = [max(endFList[0],endRList[0]), max(endFList[1],endRList[1])]
+                fragStarts = [min(startFList[0], startRList[0]), min(startFList[1], startRList[1])]
+                fragEnds = [max(endFList[0], endRList[0]), max(endFList[1], endRList[1])]
                 # this is a suspected DEL with BPs: fragEnds[0], fragStarts[1]
             else:
                 # at least one pair of F+R reads doesn't overlap, how? ignore qname
@@ -444,9 +447,9 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
             if ((startFList[1] <= endRList[1]) and (endFList[1] >= startRList[1]) and
                 (startFList[0] <= endRList[0]) and (endFList[0] >= startRList[0])):
                 # OK: first F+R reads overlap and same for second pair of F+R reads -> merge each pair
-                fragStarts = [min(startFList[1],startRList[1]), min(startFList[0],startRList[0])]
-                fragEnds = [max(endFList[1],endRList[1]), max(endFList[0],endRList[0])]
-                 # this is a suspected DUP with BPs: fragStarts[0], fragEnds[-1]
+                fragStarts = [min(startFList[1], startRList[1]), min(startFList[0], startRList[0])]
+                fragEnds = [max(endFList[1], endRList[1]), max(endFList[0], endRList[0])]
+                # this is a suspected DUP with BPs: fragStarts[0], fragEnds[-1]
             else:
                 # at least one pair of F+R reads doesn't overlap, how? ignore qname
                 return
@@ -454,22 +457,23 @@ def Qname2ExonCount(startFList,endFList,startRList,endRList,exonNCL,countsVec,ma
         else:
             # one pair is in order but the other is not, doesn't make sense
             return
-        
-   #######################################################
+
+    #######################################################
     # find exons overlapped by the fragment and increment counters
     #######################################################
     # we want to increment countsVec at most once per exon, even if
     # we have several intervals that overlap the same exon
     exonsSeen = []
     for fi in range(len(fragStarts)):
-        overlappedExons = exonNCL.find_overlap(fragStarts[fi],fragEnds[fi])
+        overlappedExons = exonNCL.find_overlap(fragStarts[fi], fragEnds[fi])
         for exon in overlappedExons:
             exonIndex = exon[2]
             if (exonIndex not in exonsSeen):
                 exonsSeen.append(exonIndex)
                 incrementCount(countsVec, exonIndex)
                 # countsVec[exonIndex] += 1
-    
+
+
 ####################################################
 # incrementCount:
 # increment the counter in countsVec (1D numpy array) at index exonIndex
