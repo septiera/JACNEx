58311888cf2983bb631efb27feaa601e356fbdf4
@septiera <amandine.septier@univ-grenoble-alpes.fr>
2023-02-10 17:50:37 +0100

    CNCalls/copyNumbersCalls.py: added output printing functions

--- CNCalls/copyNumbersCalls.py
+++ CNCalls/copyNumbersCalls.py
@@ -1,5 +1,6 @@
-import os
 import logging
+import os
+import gzip
 import numpy as np
 import numba
 import scipy.stats
@@ -13,6 +14,12 @@ import clusterSamps.genderDiscrimination
 # set up logger, using inherited config
 logger = logging.getLogger(__name__)
 
+# prevent matplotlib DEBUG messages filling the logs when we are in DEBUG loglevel
+matplotlib_logger = logging.getLogger('matplotlib')
+matplotlib_logger.setLevel(logging.WARNING)
+
+# set up logger, using inherited config
+logger = logging.getLogger(__name__)
 
 ###############################################################################
 ############################ PUBLIC FUNCTIONS #################################
@@ -145,6 +152,40 @@ def CNCalls(sex2Clust, exons, countsNorm, clusts2Samps, clusts2Ctrls, priors, SO
     PDF.close()
     return(emissionArray)
 
+#############################
+# printCallingFile:
+# Args:
+# - emissionArray (np.ndarray[float]): contain emission probabilities. dim=NbExons* (NbSOIs*[CN0,CN1,CN2,CN3+])
+# - exons (list of lists[str,int,int,str]): information on exon, containing CHR,START,END,EXON_ID
+# - SOIs (list[str]): sampleIDs copied from countsFile's header
+# - outFile is a filename that doesn't exist, it can have a path component (which must exist),
+#     output will be gzipped if outFile ends with '.gz'
+#
+# Print this data to outFile as a 'callingFile' (same format parsed by parseClustsFile).
+def printClustersFile(emissionArray, exons, SOIs, outFile):
+    try:
+        if outFile.endswith(".gz"):
+            outFH = gzip.open(outFile, "xt", compresslevel=6)
+        else:
+            outFH = open(outFile, "x")
+    except Exception as e:
+        logger.error("Cannot (gzip-)open clustersFile %s: %s", outFile, e)
+        raise Exception('cannot (gzip-)open clustersFile')
+
+    toPrint = "CHR\tSTART\tEND\tEXON_ID\t"
+    for i in SOIs:
+        for j in range(4):
+            toPrint += f"{i}_CN{j}_prob"+"\t"
+            
+    outFH.write(toPrint.rstrip())
+    for i in range(len(exons)):
+        toPrint = exons[i][0] + "\t" + str(exons[i][1]) + "\t" + str(exons[i][2]) + "\t" + exons[i][3]
+        toPrint += calls2str(emissionArray, i)
+        toPrint += "\n"
+        outFH.write(toPrint)
+    outFH.close()
+
+
 ###############################################################################
 ############################ PRIVATE FUNCTIONS ################################
 ###############################################################################
@@ -153,11 +194,11 @@ def CNCalls(sex2Clust, exons, countsNorm, clusts2Samps, clusts2Ctrls, priors, SO
 #####################################
 # allocateEmissionArrayPrivate [PRIVATE FUNCTION, DO NOT CALL FROM OUTSIDE]
 # Args:
-# - exons (dict[str, List[int]]): key: clusterID , value: samples index list
-# - SOIs (np.ndarray[float]): normalised counts
+# - exons (list of lists[str,int,int,str]): information on exon, containing CHR,START,END,EXON_ID
+# - SOIs (list[str]): sampleIDs copied from countsFile's header
 # Return:
-# - Returns an all zeroes float array, adapted for
-# storing the logOdds for each type of copy number.
+# - Returns an float array with -1, adapted for
+# storing the probabilities for each type of copy number.
 # dim= NbExons x [NbSOIs x [CN0, CN1, CN2,CN3+]]
 def allocateEmissionArrayPrivate(SOIs, exons):
     # order=F should improve performance
@@ -589,3 +630,14 @@ def filtersPiePlotPrivate(clustID, filterCounters, pdf):
 
     pdf.savefig(fig)
     matplotlib.pyplot.close()
+    
+#################################################
+# calls2str:
+# return a string holding the calls from emissionArray[exonIndex],
+# tab-separated and starting with a tab
+@numba.njit
+def calls2str(emissionArray, exonIndex):
+    toPrint = ""
+    for i in range(emissionArray.shape[1]):
+        toPrint += "\t" + "{:0.2f}".format(emissionArray[exonIndex, i])
+    return(toPrint)
