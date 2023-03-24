b3489e0fabdfdf6c208152f43853d8c294ee2092
@septiera <amandine.septier@univ-grenoble-alpes.fr>
2023-03-24 14:18:06 +0100

    clusterSamps/genderPrediction.py: redesign of the optional module, new functions

--- clusterSamps/genderPrediction.py
+++ clusterSamps/genderPrediction.py
@@ -1,6 +1,9 @@
 import numpy as np
 import logging
 
+# import sklearn submodule for Kmeans calculation
+import sklearn.cluster
+
 # set up logger, using inherited config
 logger = logging.getLogger(__name__)
 
@@ -8,28 +11,154 @@ logger = logging.getLogger(__name__)
 ###############################################################################
 ############################ PUBLIC FUNCTIONS #################################
 ###############################################################################
-
-###############################################################################
-# getGonoExIndexes:
-# - identifies the sex chromosomes (gonosomes) presents in the "countsFile"
-# input data and extracts the associated exon indexes.
-# Please note that in this script only X, Y, Z and W chromosomes can be found.
-# This is more than enough to cover a large part of the living world
-# => mammals, birds, fish, reptiles.
+# sexAssignment
+# from gonosome exon definitions identify the species concerned: mammals or others
+# (birds, reptiles, amphibians, fish)
+# Performs a kmeans on the gonosome exon counts, to identify two groups associated
+# with the sexe.
+# Assigns sexe to each kmean groups based on their coverage ratios.
 #
 # Args:
-#  - exons (list of lists[str,int,int,str]): information on exon, containing CHR,START,END,EXON_ID
-# Returns a boolean numpy.ndarray where the indices of the exons
-# corresponding to the gonosomes are True and False for the autosomes.
-def getGonoExIndexes(exons):
-    # sex chromosome list
-    gonoChromList = ["X", "Y", "W", "Z"]
+# - gonosomesFPM (np.ndarray[float]): normalized fragment counts
+# - gonosomesExons (list of list[str]): exon definitions
+# - samples (list[str]): sample names
+# Returns a list of lists[str] for each sexe is assigned the Kmeans samples list.
+def sexAssignment(gonosomesFPM, gonosomesExons, samples):
+    # To Fill and return
+    sexAssig = []
 
-    # cases where the chromosome terminology does not match the sex chromosome list.
-    # adding chr in front of names
-    if exons[0][0].startswith("chr"):
+    # Hard coded variables
+    # most species are bisexual: Female vs Male
+    nbSexToPred = 2
+    # gonosomes list to cover different species
+    gonoChromList = ["X", "Y", "W", "Z"]
+    # empty status to assign a boolean if the data comes from mammals => True
+    # or others => False
+    mammals = None
+
+    #####
+    # species identification
+    # Know which gonosomes operate if X and Y => mammals,
+    # if Z and W => others
+    # check that the chromosome pairs are correct if not the assignment cannot be made
+    gonoNames = list(set([exon[0] for exon in gonosomesExons]))
+    if gonoNames[0].startswith("chr"):
         gonoChromList = ["chr" + letter for letter in gonoChromList]
 
-    # creation of the boolean numpy.ndarray of exonsNb length
-    chrSexMask = np.array([exon[0] in gonoChromList for exon in exons])
-    return chrSexMask
+    if sorted(gonoNames) == sorted(gonoChromList[:2]):
+        mammals = True
+    elif sorted(gonoNames) == sorted(gonoChromList[2:]):
+        mammals = False
+    else:
+        logger.error("No predefined gonosomes are present in the exon list (X, Y, Z, W ). Please check that the original BED file.")
+        raise Exception()
+
+    #####
+    # Performs an empirical method (kmeans) to dissociate male and female.
+    # The classical EM-style algorithm used by default is "lloyd"
+    kmeans = sklearn.cluster.KMeans(n_clusters=nbSexToPred).fit(gonosomesFPM.T).predict(gonosomesFPM.T)
+
+    #####
+    # ratios per Kmeans group and per gonosome calculated
+    ratioGono = getRatioGono2KmeanGp(gonoNames, kmeans, gonosomesFPM, gonosomesExons)
+
+    #####
+    # sexe assignment for each kmeans groups
+    if mammals:
+        pred = sexAssignmentPrivate(ratioGono, gonoNames, "Y", "X", ["M", "F"])
+    else:
+        pred = sexAssignmentPrivate(ratioGono, gonoNames, "Z", "W", ["F", "M"])
+
+    #####
+    # fill output results
+    for indPred in range(len(pred)):
+        samps = [samples[i] for i in range(len(kmeans)) if kmeans[i] == indPred]
+        listToFill = [pred[indPred], samps]
+        sexAssig.append(listToFill)
+
+    return sexAssig
+
+
+###############################################################################
+############################ PRIVATE FUNCTIONS ################################
+###############################################################################
+# getRatioGono2KmeanGp
+# cover ratio = median(sums of samples counts in a Kmeans group) for one gonosome
+# Args:
+# - GonoNames (list[str])
+# - kmeans (list[int]): sampsNB length where for each sample index in 'samples'
+# is associated a group predicted by Kmeans (always 0 or 1)
+# - gonosomesFPM (np.ndarray(float))
+# - gonosomesExons (list of lists[str])
+# Returns a np.ndarray[floats] contains the cover ratio, dim= GonosomesNB * kmeanGpNB
+def getRatioGono2KmeanGp(gonoNames, kmeans, gonosomesFPM, gonosomesExons):
+    # sorting of Kmeans group identifiers from 0
+    groupList = sorted(np.unique(kmeans))
+
+    # To Fill and returns
+    ratioGono = np.empty((len(gonoNames), len(groupList)), dtype=np.float32)
+
+    for gonoIndex in range(len(gonoNames)):
+        gonoName = gonoNames[gonoIndex]
+        for kmeanGroup in groupList:
+            #####################
+            # selection normalized count data on current gonosome exons (row) and samples
+            # in current Kmeans group (column)
+            exonsIndex = [i for i, exon in enumerate(gonosomesExons) if exon[0] == gonoName]
+            sampsIndexKGp = np.where(kmeans == kmeanGroup)[0]
+            gonoTmpArray = gonosomesFPM[exonsIndex][:, sampsIndexKGp]
+
+            #####################
+            # cover ratio calculation (axis=0, sum all exons for each sample)
+            ratioGono[gonoIndex, kmeanGroup] = np.median(np.sum(gonoTmpArray, axis=0))
+
+    return ratioGono
+
+
+#############################################################
+# sexAssignmentPrivate
+# Two independent predictions on the coverage ratios are performed:
+# 1) identification of sexe with a specific gonosome(eg: human Male= chrY)
+# One group is expected to have a 10 x higher ratio of this gonosome than the other sexe.
+# 2) identification of sexe with two identical gonosomes (eg: human Female = 2*chrX)
+# One groups(G1) is expected to have : 1.5*ratio(G2)<ratio(G1)< 3*ratio(G2) for this gonosome.
+# Args:
+# - ratioGono (np.ndarray[floats]): cover ratio, dim = gonosomesNB * kmeansGpNB
+# - gonoNames (list[str])
+# - chrSexUniq [str]: name of the single sex-specific gonosome (eg: human Male= "chrY", bird Female= "chrW")
+# - chrSexDup [str]: name of the duplicated sex-specific gonosome (eg: human Female= "chrX", bird Male= "chrZ")
+# - sexNames (list[str]): names of the sexes in order: 1) sex with a single gonosome 2) sex with a double gonosome
+# (eg: human ["M","F"], bird ["F","M"])
+# returns a list[str] of sex "F", "M" in the order of the Kmeans groups [0,1] in case
+# the two predictions agree otherwise returns an exception and the assignment cannot be performed
+def sexAssignmentPrivate(ratioGono, gonoNames, chrSexUniq, chrSexDup, sexNames):
+    # To Fill and return
+    pred1 = []
+    pred2 = []
+
+    for i in range(len(ratioGono)):
+        # first prediction
+        # the first group has a ratio greater than 10x the second group 
+        # => the first group is associated with the sex with a single specific gonosome
+        # if not, reverse the list
+        if ((gonoNames[i] == "chr" + chrSexUniq) or (gonoNames[i] == chrSexUniq)):
+            if (ratioGono[i][0] > (ratioGono[i][1] * 10)):
+                pred1 = sexNames[::-1]  # allows to create a new list without touching the original list
+            else:
+                pred1 = sexNames
+        # second prediction
+        # 
+        if ((gonoNames[i] == "chr" + chrSexDup) or (gonoNames[i] == chrSexDup)):
+            if (ratioGono[i][0] > (3 * ratioGono[i][1] / 2)) and (ratioGono[i][0] < (3 * ratioGono[i][1])):
+                pred2 = sexNames[::-1]
+            else:
+                pred2 = sexNames
+
+    if (pred1 == pred2):
+        return pred1
+
+    else:
+        logger.error("gender predictions are not in agreement.\n \
+            condition n°1, one gender is characterised by a specific gonosome: %s \n \
+                condition n°2 that the other gender is characterised by 2 same gonosome copies: %s ", pred1, pred2)
+        raise Exception()
