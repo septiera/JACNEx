a38a33f56a2eb3fb15b2ee384e16d0d3eaaa1d9e
@septiera <amandine.septier@univ-grenoble-alpes.fr>
2023-06-09 13:56:01 +0200

    s3_CNCalls.py: parallelization code added (non-functional), extractCNCallsFromPrev function argument changed

--- s3_CNCalls.py
+++ s3_CNCalls.py
@@ -8,11 +8,14 @@
 import sys
 import getopt
 import os
+import math
 import time
 import logging
+from concurrent.futures import ProcessPoolExecutor
 
 ####### MAGE-CNV modules
 import countFrags.countsFile
+import clusterSamps.getGonosomesExonsIndexes
 import clusterSamps.clustFile
 import clusterSamps.clustering
 import CNCalls.CNCallsFile
@@ -44,6 +47,8 @@ def parseArgs(argv):
     prevClustsFile = ""
     plotFolder = ""
     samps2Check = ""
+    # jobs default: 80% of available cores
+    jobs = round(0.8 * len(os.sched_getaffinity(0)))
 
     usage = "NAME:\n" + scriptName + """\n
 DESCRIPTION:
@@ -71,13 +76,14 @@ ARGUMENTS:
             for samples contained in immutable clusters between old and new versions of the clustering files.
     --prevclusts [str] optional: pre-existing clustering file produced by s2_clusterSamps.py for the same
             timestamp as the pre-existing copy number call file.
+    --jobs [int] : cores that we can use, defaults to 80% of available cores ie """ + str(jobs) + "\n" + """
     --plotFolder [str]: subdir (created if needed) where result plots files will be produced, wish to monitor the filters
     --samps2Check [str]: comma-separated list of sample names, to plot calls, must be passed with --plotDir
     -h , --help: display this help and exit\n"""
 
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', ["help", "counts=", "clusts=", "out=", "prevcncalls=",
-                                                           "prevclusts=", "plotDir=", "samps2Check="])
+                                                           "prevclusts=", "jobs=", "plotDir=", "samps2Check="])
     except getopt.GetoptError as e:
         raise Exception(e.msg + ". Try " + scriptName + " --help")
     if len(args) != 0:
@@ -97,6 +103,8 @@ ARGUMENTS:
             prevCNCallsFile = value
         elif (opt in ("--prevclusts")):
             prevClustsFile = value
+        elif opt in ("--jobs"):
+            jobs = value
         elif (opt in ("--plotDir")):
             plotFolder = value
         elif (opt in ("--samps2Check")):
@@ -134,6 +142,13 @@ ARGUMENTS:
     if (prevClustsFile != "") and (not os.path.isfile(prevClustsFile)):
         raise Exception("previous clustering File " + prevClustsFile + " doesn't exist")
 
+    try:
+        jobs = int(jobs)
+        if (jobs <= 0):
+            raise Exception()
+    except Exception:
+        raise Exception("jobs must be a positive integer, not " + str(jobs))
+
     if (plotFolder == "" and samps2Check != ""):
         raise Exception("you must use --checkSamps with --plotDir, not independently. Try " + scriptName + " --help")
 
@@ -148,7 +163,7 @@ ARGUMENTS:
             samps2Check = samps2Check.split(",")
 
     # AOK, return everything that's needed
-    return(countsFile, newClustsFile, outFile, prevCNCallsFile, prevClustsFile, plotFolder, samps2Check)
+    return(countsFile, newClustsFile, outFile, prevCNCallsFile, prevClustsFile, jobs, plotFolder, samps2Check)
 
 
 ###############################################################################
@@ -162,7 +177,7 @@ ARGUMENTS:
 # may be available in the log
 def main(argv):
     # parse, check and preprocess arguments
-    (countsFile, clustsFile, outFile, prevCNCallsFile, prevClustsFile, plotFolder, samps2Check) = parseArgs(argv)
+    (countsFile, clustsFile, outFile, prevCNCallsFile, prevClustsFile, jobs, plotFolder, samps2Check) = parseArgs(argv)
 
     # args seem OK, start working
     logger.debug("called with: " + " ".join(argv[1:]))
@@ -195,67 +210,106 @@ def main(argv):
     ######################################################
     # allocate CNcallsArray, and populate it with pre-calculated observed probabilities
     # if CNCallsFile and prevClustFile are provided.
-    # also returns a boolean np.array to identify the samples to be reanalysed if the clusters change
+    # also returns a boolean np.array to identify the clusters to be reanalysed if the clusters change
     try:
-        (CNCallsArray, callsFilled) = CNCalls.CNCallsFile.extractObservedProbsFromPrev(exons, samples, clusters, prevCNCallsFile, prevClustsFile)
+        (CNCallsArray, callsFilled) = CNCalls.CNCallsFile.extractCNCallsFromPrev(exons, samples, clusters, prevCNCallsFile, prevClustsFile)
     except Exception as e:
-        raise Exception("extractObservedProbsFromPrev failed - " + str(e))
+        raise Exception("extractCNCallsFromPrev failed - " + str(e))
 
     thisTime = time.time()
     logger.debug("Done parsing previous CNCallsFile and prevClustFile, in %.2fs", thisTime - startTime)
     startTime = thisTime
+    
+    # if CNCalls fails for any clusters, we have to remember their indexes
+    # and only expunge them at the end -> save their indexes in failedClusters
+    failedClusters = []
 
     # total number of samples that still need to be processed
-    nbOfSamplesToProcess = len(samples)
-    for samplesIndex in range(len(samples)):
-        if callsFilled[samplesIndex]:
-            nbOfSamplesToProcess -= 1
+    nbOfClustersToProcess = len(clusters)
+    for clustIndex in range(len(clusters)):
+        if callsFilled[clustIndex]:
+            nbOfClustersToProcess -= 1
 
-    if nbOfSamplesToProcess == 0:
-        logger.info("all provided samples are in previous callsFile and clusters are the same, not producing a new one")
+    if nbOfClustersToProcess == 0:
+        logger.info("all provided clusters are identical to the previous callsFile, not producing a new one")
     else:
         ####################################################
         # CN Calls
         ##############################
+        # decide how the work will be parallelized
+        # we are allowed to use jobs cores in total: we will process paraClusters clusters in
+        # parallel, each sample will be processed using coresPerSample.
+        # -> we target targetCoresPerSample coresPerSample, this is increased if we
+        #    have few samples to process (and we use ceil() so we may slighty overconsume)
+        targetCoresPerCluster = 3
+        paraClusters = min(math.ceil(jobs / targetCoresPerCluster), len(clusters))
+        coresPerCluster = math.ceil(jobs / paraClusters)
+        logger.info("%i new cluster => will process %i in parallel, using up to %i cores/cluster",
+                    len(clusters), paraClusters, coresPerCluster)
+
         # identifying autosomes and gonosomes "exons" index
         # recall clusters are derived from autosomal or gonosomal analyses
         try:
-            maskGExIndexes = clusterSamps.getGonosomesExonsIndexes.getSexChrIndexes(exons)
+            maskSourceExons = clusterSamps.getGonosomesExonsIndexes.getSexChrIndexes(exons)
         except Exception as e:
             raise Exception("getSexChrIndexes failed %s", e)
 
+        #####################################################
+        # Define nested callback for processing CNCalls() result (so CNCallsArray et al
+        # are in its scope)
+
+        # mergeCalls:
+        # arg: a Future object returned by ProcessPoolExecutor.submit(CNCalls.copyNumbersCalls.CNCalls).
+        # CNCalls() returns a 4-element tuple (clustID, colIndInCNCallsArray, sourceExons, clusterCalls).
+        # If something went wrong, log and populate failedClusters;
+        # otherwise fill column at index colIndInCNCallsArray and row at index sourceExons in CNCallsArray
+        # with calls stored in clusterCalls
+        def mergeCalls(futurecounts2callsRes):
+            e = futurecounts2callsRes.exception()
+            if e is not None:
+                #  exceptions raised by CNCalls are always Exception(str(sampleIndex))
+                si = int(str(e))
+                logger.warning("Failed to CNCalls for cluster n° %s, skipping it", clusters[si])
+                failedClusters.append(si)
+            else:
+                counts2callsRes = futurecounts2callsRes.result()
+                si = counts2callsRes[0]
+                for exonIndex in range(len(counts2callsRes[1])):
+                    CNCallsArray[counts2callsRes[2][exonIndex], counts2callsRes[1]] = counts2callsRes[3][exonIndex]
+
+                logger.info("Done copy number calls for cluster n°%s", counts2callsRes[0])
+
         # To be parallelised => browse clusters
-        for clustID in range(len(clusters)):
-            logger.info("#### Process cluster n°%i", clustID)
+        with ProcessPoolExecutor(paraClusters) as pool:
+            for clustID in range(len(clusters)):
+                logger.info("#### Process cluster n°%i", clustID)
 
-            ##### validity sanity check
-            if validity[clustID] == 0:
-                logger.warning("cluster %s is invalid, low sample number", clustID)
-                continue
+                ##### validity sanity check
+                if validity[clustID] == 0:
+                    logger.warning("cluster %s is invalid, low sample number", clustID)
+                    continue
 
-            ##### previous data sanity filters
-            # test if the samples of the cluster have already been analysed
-            # and the filling of CNcallsArray has been done
-            sub_t = callsFilled[clusters[clustID]]
-            if all(sub_t):
-                logger.info("samples in cluster %s, already filled from prevCallsFile", clustID)
-                continue
+                ##### previous data sanity filters
+                # test if the cluster has already been analysed
+                # and the filling of CNcallsArray has been done
+                if callsFilled[clustID]:
+                    logger.info("samples in cluster %s, already filled from prevCallsFile", clustID)
+                    continue
 
-            ##### run prediction for current cluster
-            try:
-                CNCalls.copyNumbersCalls.CNCalls(CNCallsArray, clustID, exonsFPM, intergenicsFPM,
-                                                 samples, exons, clusters, ctrlsClusters,
-                                                 sourceClusters, maskGExIndexes, plotFolder, samps2Check)
-            except Exception as e:
-                raise Exception("CNCalls failed: ", e)
+                ##### run prediction for current cluster
+                futureRes  = pool.submit(CNCalls.copyNumbersCalls.CNCalls(clustID, exonsFPM, intergenicsFPM, samples,
+                                                                          exons, clusters, ctrlsClusters, sourceClusters,
+                                                                          maskSourceExons, plotFolder, samps2Check))
+                
+                futureRes.add_done_callback(mergeCalls)
 
-            thisTime = time.time()
-            logger.debug("Done Copy Number Exons Calls for cluster n°%s, in %.2f s", clustID, thisTime - startTime)
-            startTime = thisTime
+                thisTime = time.time()
+                logger.debug("Done Copy Number Exons Calls for cluster n°%s, in %.2f s", clustID, thisTime - startTime)
+                startTime = thisTime
 
         #####################################################
         # Print exon defs + calls to outFile
-        CNCalls.CNCallsFile.printCNCallsFile(CNCallsArray, exons, samples, outFile)
+        CNCalls.CNCallsFile.printCNCallsFile(CNCallsArray, exons, clusters, outFile)
 
         thisTime = time.time()
         logger.debug("Done printing calls for all (non-failed) samples, in %.2fs", thisTime - startTime)
@@ -266,16 +320,17 @@ def main(argv):
 ######################################## Main ######################################
 ####################################################################################
 if __name__ == '__main__':
+    scriptName = os.path.basename(sys.argv[0])
     # configure logging, sub-modules will inherit this config
     logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.DEBUG)
     # set up logger: we want script name rather than 'root'
-    logger = logging.getLogger(os.path.basename(sys.argv[0]))
+    logger = logging.getLogger(scriptName)
 
     try:
         main(sys.argv)
     except Exception as e:
         # details on the issue should be in the exception name, print it to stderr and die
-        sys.stderr.write("ERROR in " + sys.argv[0] + " : " + str(e) + "\n")
+        sys.stderr.write("ERROR in " + scriptName + " : " + str(e) + "\n")
         sys.exit(1)
