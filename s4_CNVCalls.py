24bc68b8b3886fe704b9ba7fe6d3f43799e2b418
septiera <amandine.septier@univ-grenoble-alpes.fr>
2023-08-04 14:00:18 +0200

    s4_CNVCalls.py: script overhauled, new objectives

--- s4_CNVCalls.py
+++ s4_CNVCalls.py
@@ -1,9 +1,9 @@
 ###############################################################################################
-######################################## MAGE-CNV step 4:  call grouping     ##################
+################################ JACNEx  step 4:  CNV calling #################################
 ###############################################################################################
-# Given a TSV of likelihoods per exon for 4 copy number states [CN0,CN1,CN2,CN3+] for each
-# sample and TSV files of breakpoints for each sample, obtain a VCF file of copy number variations
-# for all samples. See usage for more details.
+# Given three TSV files containing fragment counts, sample clusters, and distribution parameters
+# for fitting the copy number profile, obtain a VCF file of copy number variations for all samples.
+# See usage for more details.
 ###############################################################################################
 import sys
 import getopt
@@ -12,7 +12,9 @@ import time
 import logging
 import numpy as np
 
-####### MAGE-CNV modules
+####### JACNEx modules
+import countFrags.countsFile
+import clusterSamps.clustFile
 import CNCalls.CNCallsFile
 import groupCalls.transitions
 import groupCalls.HMM
@@ -35,39 +37,45 @@ def parseArgs(argv):
     scriptName = os.path.basename(argv[0])
 
     # mandatory args
-    callsFile = ""
-    BPFolder = ""
+    countsFile = ""
+    clustsFile = ""
+    paramsFile = ""
     outFile = ""
     # optionnal args with default values
-    priors = np.array([6.34e-4, 2.11e-3, 9.96e-1, 1.25e-3])
-    CNStatus = ["CN0", "CN1", "CN2", "CN3"]
+    BPFolder = ""
     padding = 10
 
     usage = "NAME:\n" + scriptName + """\n
 
 DESCRIPTION:
-Given a TSV of likelihoods per exon for 4 copy number states [CN0,CN1,CN2,CN3+]
-for each sample and TSV files of breakpoints for each sample.
-Accurately identifies the CNs for each exon via the hidden markov chain (HMM)
-algorithm by weighting the transitions with the lengths separating the exons
-and by weighting the data-based transition matrix with the priors found in the literature.
-Group exons to form CNVs, taking into account no-call exons.
-Adjustment of precise delimitations if breakpoints have already been observed.
-Produces a VCF file of copy number variations for all samples.
+Given three TSV files containing fragment counts, sample clusters, and distribution parameters
+for fitting the copy number profile:
+It calculates likelihoods for each sample and copy number, performs a hidden Markov chain
+to obtain the best predictions, groups exons to form copy number variants (CNVs),
+and deduces exact breakpoints if possible.
+Finally, it generates a VCF (Variant Call Format) file for all analyzed samples.
 
 ARGUMENTS:
-    --calls [str]: TSV file, first 4 columns hold the exon definitions, subsequent columns
-            hold the likelihoods per exon for 4 copy number states [CN0,CN1,CN2,CN3+]
-            for each sample. File obtained from s3_CNCalls.py.
-    --BPFolder [str]: folder containing gzipped or ungzipped TSV for all samples analysed.
-                      Files obtained from s1_countFrags.py
+    --counts [str]: TSV file, first 4 columns hold the exon definitions, subsequent columns
+                    hold the fragment counts. File obtained from 1_countFrags.py.
+    --clusts [str]: TSV file, contains 5 columns hold the sample cluster definitions.
+                    [clusterID, sampsInCluster, controlledBy, validCluster, clusterStatus]
+                    File obtained from 2_clusterSamps.py.
+    --params [str]: TSV file contains exon definitions in its first four columns,
+                    followed by distribution parameters ["loc", "scale"] for exponential
+                    and Gaussian distributions, and an additional column indicating the
+                    exon filtering status for each cluster.
+                    The file is generated using the 3_CNDistParams.py script.
     --out [str]: file where results will be saved, must not pre-exist, will be gzipped if it ends
                  with '.gz', can have a path component but the subdir must exist
+    --BPFolder [str]: folder containing gzipped or ungzipped TSV for all samples analysed.
+                      Files obtained from s1_countFrags.py
     --padding [int] : number of bps used to pad the exon coordinates, default : """ + str(padding) + """
     -h , --help: display this help and exit\n"""
 
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', ["help", "calls=", "BPFolder=", "out=", "padding="])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', ["help", "counts=", "clusts=", "params=", "out=",
+                                                           "BPFolder=", "padding="])
     except getopt.GetoptError as e:
         raise Exception(e.msg + ". Try " + scriptName + " --help")
     if len(args) != 0:
@@ -77,12 +85,16 @@ ARGUMENTS:
         if (opt in ('-h', '--help')):
             sys.stderr.write(usage)
             sys.exit(0)
-        elif (opt in ("--calls")):
-            callsFile = value
-        elif (opt in ("--BPFolder")):
-            BPFolder = value
+        elif (opt in ("--counts")):
+            countsFile = value
+        elif (opt in ("--clusts")):
+            clustsFile = value
+        elif (opt in ("--params")):
+            paramsFile = value
         elif opt in ("--out"):
             outFile = value
+        elif (opt in ("--BPFolder")):
+            BPFolder = value
         elif opt in ("--padding"):
             padding = value
         else:
@@ -90,15 +102,27 @@ ARGUMENTS:
 
     #####################################################
     # Check that the mandatory parameter is present
-    if callsFile == "":
-        raise Exception("you must provide a calls file with --calls. Try " + scriptName + " --help.")
-    elif (not os.path.isfile(callsFile)):
-        raise Exception("callsFile " + callsFile + " doesn't exist.")
+    if countsFile == "":
+        raise Exception("you must provide a counts file with --counts. Try " + scriptName + " --help.")
+    elif (not os.path.isfile(countsFile)):
+        raise Exception("countsFile " + countsFile + " doesn't exist.")
 
-    if BPFolder == "":
-        raise Exception("you must provide a breakpoints folder use --BPFolder. Try " + scriptName + " --help.")
-    elif (not os.path.isdir(BPFolder)):
-        raise Exception("BreakPoints folder " + BPFolder + " doesn't exist.")
+    if clustsFile == "":
+        raise Exception("you must provide a clustering results file use --clusts. Try " + scriptName + " --help.")
+    elif (not os.path.isfile(clustsFile)):
+        raise Exception("clustsFile " + clustsFile + " doesn't exist.")
+    
+    if paramsFile == "":
+        raise Exception("you must provide a continuous distribution parameters file use --params. Try " + scriptName + " --help.")
+    elif (not os.path.isfile(paramsFile)):
+        raise Exception("paramsFile " + paramsFile + " doesn't exist.")
+
+    if outFile == "":
+        raise Exception("you must provide an outFile with --out. Try " + scriptName + " --help")
+    elif os.path.exists(outFile):
+        raise Exception("outFile " + outFile + " already exists")
+    elif (os.path.dirname(outFile) != '') and (not os.path.isdir(os.path.dirname(outFile))):
+        raise Exception("the directory where outFile " + outFile + " should be created doesn't exist")
 
     #####################################################
     # Check other argsjobs = round(0.8 * len(os.sched_getaffinity(0)))
@@ -109,6 +133,9 @@ ARGUMENTS:
     elif (os.path.dirname(outFile) != '') and (not os.path.isdir(os.path.dirname(outFile))):
         raise Exception("the directory where outFile " + outFile + " should be created doesn't exist")
 
+    if os.path.isdir(BPFolder):
+        print("TODO dev BPFolder treatments")
+
     try:
         padding = int(padding)
         if (padding < 0):
@@ -117,7 +144,7 @@ ARGUMENTS:
         raise Exception("padding must be a non-negative integer, not " + str(padding))
 
     # AOK, return everything that's needed
-    return(callsFile, CNStatus, priors, BPFolder, outFile, padding)
+    return(countsFile, clustsFile, paramsFile, outFile, BPFolder, padding)
 
 
 ###############################################################################
@@ -130,93 +157,102 @@ ARGUMENTS:
 # If anything goes wrong, raise Exception("SOME EXPLICIT ERROR MESSAGE"), more details
 # may be available in the log
 def main(argv):
+  
     # parse, check and preprocess arguments
-    (callsFile, CNStatus, priors, BPFolder, outFile, padding) = parseArgs(argv)
+    (countsFile, clustsFile, paramsFile, outFile, BPFolder, padding) = parseArgs(argv)
+    
+    # # --CN [str]: TXT file contains two lines separated by tabulations.
+    # # The first line consists of entries naming the default copy number status,
+    # # default: """ + CNStatus + """, and the second line contains the prior
+    # # probabilities of occurrence for the events, default: """ + priors + """
+    # # obtained from 1000 genome data (doi:10.1186/1471-2105-13-305).
+    # CNStatus = ["CN0", "CN1", "CN2", "CN3"]
+    # priors = np.array([6.34e-4, 2.11e-3, 9.96e-1, 1.25e-3])
 
-    # args seem OK, start working
-    logger.debug("called with: " + " ".join(argv[1:]))
-    logger.info("starting to work")
-    startTime = time.time()
+    # # args seem OK, start working
+    # logger.debug("called with: " + " ".join(argv[1:]))
+    # logger.info("starting to work")
+    # startTime = time.time()
 
-    #############
-    # parse calls
-    try:
-        (exons, samples, CNCallsArray) = CNCalls.CNCallsFile.parseCNcallsFile(callsFile, len(CNStatus))
-    except Exception as e:
-        logger.error("parseCNcallsFile failed for %s : %s", callsFile, repr(e))
-        raise Exception("parseCNcallsFile failed")
+    # #############
+    # # parse calls
+    # try:
+    #     (exons, samples, CNCallsArray) = CNCalls.CNCallsFile.parseCNcallsFile(callsFile, len(CNStatus))
+    # except Exception as e:
+    #     logger.error("parseCNcallsFile failed for %s : %s", callsFile, repr(e))
+    #     raise Exception("parseCNcallsFile failed")
 
-    thisTime = time.time()
-    logger.debug("Done parse callsFile, in %.2fs", thisTime - startTime)
-    startTime = thisTime
+    # thisTime = time.time()
+    # logger.debug("Done parse callsFile, in %.2fs", thisTime - startTime)
+    # startTime = thisTime
 
-    ############
-    # obtaining a list of observations and a transition matrix based on the data.
-    try:
-        transMatrix = groupCalls.transitions.getTransMatrix(CNCallsArray, priors, samples, CNStatus, os.path.dirname(outFile))
-    except Exception as e:
-        logger.error("getTransMatrix failed : %s", repr(e))
-        raise Exception("getTransMatrix failed")
+    # ############
+    # # obtaining a list of observations and a transition matrix based on the data.
+    # try:
+    #     transMatrix = groupCalls.transitions.getTransMatrix(CNCallsArray, priors, samples, CNStatus, os.path.dirname(outFile))
+    # except Exception as e:
+    #     logger.error("getTransMatrix failed : %s", repr(e))
+    #     raise Exception("getTransMatrix failed")
 
-    thisTime = time.time()
-    logger.debug("Done getTransMatrix, in %.2fs", thisTime - startTime)
-    startTime = thisTime
+    # thisTime = time.time()
+    # logger.debug("Done getTransMatrix, in %.2fs", thisTime - startTime)
+    # startTime = thisTime
 
-    #############
-    # CNVs calls
-    try:
-        for sampIndex in range(len(samples)):
-            # Extract the CN calls for the current sample
-            CNcallOneSamp = CNCallsArray[:, sampIndex * len(CNStatus): sampIndex * len(CNStatus) + len(CNStatus)]
-            try:
-                # Perform CNV inference using HMM
-                CNVsSampList = groupCalls.HMM.inferCNVsUsingHMM(CNcallOneSamp, exons, transMatrix, priors)
-            except Exception as e:
-                logger.error("inferCNVsUsingHMM failed : %s", repr(e))
-                raise Exception("inferCNVsUsingHMM failed")
+    # #############
+    # # CNVs calls
+    # try:
+    #     for sampIndex in range(len(samples)):
+    #         # Extract the CN calls for the current sample
+    #         CNcallOneSamp = CNCallsArray[:, sampIndex * len(CNStatus): sampIndex * len(CNStatus) + len(CNStatus)]
+    #         try:
+    #             # Perform CNV inference using HMM
+    #             CNVsSampList = groupCalls.HMM.inferCNVsUsingHMM(CNcallOneSamp, exons, transMatrix, priors)
+    #         except Exception as e:
+    #             logger.error("inferCNVsUsingHMM failed : %s", repr(e))
+    #             raise Exception("inferCNVsUsingHMM failed")
 
-            # Create a column with sampIndex values
-            sampIndexColumn = np.full((CNVsSampList.shape[0], 1), sampIndex, dtype=CNVsSampList.dtype)
-            # Concatenate CNVsSampList with sampIndex column
-            CNVsSampListAddSampInd = np.hstack((CNVsSampList, sampIndexColumn))
-            # Stack CNVsSampList_with_sampIndex vertically with previous results
-            CNVArray = np.vstack((CNVArray, CNVsSampListAddSampInd))
+    #         # Create a column with sampIndex values
+    #         sampIndexColumn = np.full((CNVsSampList.shape[0], 1), sampIndex, dtype=CNVsSampList.dtype)
+    #         # Concatenate CNVsSampList with sampIndex column
+    #         CNVsSampListAddSampInd = np.hstack((CNVsSampList, sampIndexColumn))
+    #         # Stack CNVsSampList_with_sampIndex vertically with previous results
+    #         CNVArray = np.vstack((CNVArray, CNVsSampListAddSampInd))
 
-    except Exception as e:
-        logger.error("CNVs calls failed : %s", repr(e))
-        raise Exception("CNVs calls failed")
+    # except Exception as e:
+    #     logger.error("CNVs calls failed : %s", repr(e))
+    #     raise Exception("CNVs calls failed")
 
-    thisTime = time.time()
-    logger.debug("Done CNVs calls, in %.2fs", thisTime - startTime)
-    startTime = thisTime
+    # thisTime = time.time()
+    # logger.debug("Done CNVs calls, in %.2fs", thisTime - startTime)
+    # startTime = thisTime
 
-    ############
-    # vcf format
-    try:
-        resVcf = groupCalls.HMM.CNV2Vcf(CNVArray, exons, samples, padding)
-    except Exception as e:
-        logger.error("CNV2Vcf failed : %s", repr(e))
-        raise Exception("CNV2Vcf failed")
+    # ############
+    # # vcf format
+    # try:
+    #     resVcf = groupCalls.HMM.CNV2Vcf(CNVArray, exons, samples, padding)
+    # except Exception as e:
+    #     logger.error("CNV2Vcf failed : %s", repr(e))
+    #     raise Exception("CNV2Vcf failed")
 
-    thisTime = time.time()
-    logger.debug("Done CNV2Vcf, in %.2fs", thisTime - startTime)
-    startTime = thisTime
+    # thisTime = time.time()
+    # logger.debug("Done CNV2Vcf, in %.2fs", thisTime - startTime)
+    # startTime = thisTime
 
-    ###########
-    # print results
-    try:
-        groupCalls.HMM.printVcf(resVcf, outFile, scriptName, samples)
-    except Exception as e:
-        logger.error("printVcf failed : %s", repr(e))
-        raise Exception("printVcf failed")
+    # ###########
+    # # print results
+    # try:
+    #     groupCalls.HMM.printVcf(resVcf, outFile, scriptName, samples)
+    # except Exception as e:
+    #     logger.error("printVcf failed : %s", repr(e))
+    #     raise Exception("printVcf failed")
 
-    thisTime = time.time()
-    logger.debug("Done printVcf, in %.2fs", thisTime - startTime)
-    startTime = thisTime
+    # thisTime = time.time()
+    # logger.debug("Done printVcf, in %.2fs", thisTime - startTime)
+    # startTime = thisTime
 
-    thisTime = time.time()
-    logger.debug("Done printing groupingCalls, in %.2fs", thisTime - startTime)
-    logger.info("ALL DONE")
+    # thisTime = time.time()
+    # logger.debug("Done printing groupingCalls, in %.2fs", thisTime - startTime)
+    # logger.info("ALL DONE")
 
 
 ####################################################################################
