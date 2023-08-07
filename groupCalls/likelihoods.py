c4fdb3ec6ed8c95afa41cf7014ad6b09f1ecf889
septiera <amandine.septier@univ-grenoble-alpes.fr>
2023-08-07 20:53:42 +0200

    groupCalls/likelihoods.py: development of the allocateLikelihoodsArray and observationCounts2Likelihoods functions, addition of a getDistributionParams sub-function

--- groupCalls/likelihoods.py
+++ groupCalls/likelihoods.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+import scipy.stats
 
 # prevent PIL flooding the logs when we are in DEBUG loglevel
 logging.getLogger('PIL').setLevel(logging.WARNING)
@@ -18,10 +19,13 @@ logger = logging.getLogger(__name__)
 # - numSamps [int]: number of samples to process
 # - numExons [int]: number of exons to process
 # - numCN [int]: number of copy numbers to process (default = 4 , CN0,CN1,CN2,CN3+)
-# 
+#
 # Returns an float array with -1, allocated likelihoods for each
 # individual and copy number combination for each exons.
 # dim = nbOfExons * (nbOfSamps * nbOfCN)
+def allocateLikelihoodsArray(numSamps, numExons, numCN):
+    # order=F should improve performance
+    return np.full((numExons, (numSamps * numCN)), -1, dtype=np.float64, order='F')
 
 
 ######################################
@@ -46,14 +50,86 @@ logger = logging.getLogger(__name__)
 # - exonCN2Params (np.ndarray): contains CN2 parameters (gaussian distribution loc=mean, scale=stdev)
 #                               and exon filtering status for each cluster.
 #                               Dim: nbOfExons * (nbOfClusters * ["loc", "scale", "filterStatus"]).
+# - numCNs [int]: 4 copy number status: ["CN0", "CN1", "CN2", "CN3"]
+# - numParamsCols [int]: ["loc", "scale", "filterStatus"]
 #
 # Returns a tupple (sampLikelihoodsColInd, sampLikelihoodArray):
-# - sampLikelihoodsColInd (list[ints]): Column indexes to fill in the large likelihood array.
-# - sampLikelihoodArray (np.ndarray[floats]): Likelihoods for all CN types for cluster samples.
+# - relevantCols (list[ints]): column indices in the larger array of all analyzed samples,
+#                              where each column corresponds to a specific copy number type (CN0, CN1, CN2, CN3)
+#                              for the given clusterID and samples.
+#                              These column indices are used to store the likelihoods in the appropriate
+#                              positions of the larger array outside the function.
+# - relevantRows (list[ints]): exon indexes in the likelihoodArray corresponding to exons
+#                              with filtering status 4 ("Calls") for the given clusterID.
+#                              These rows represent the exons for which likelihoods are calculated.
+# - likelihoodArray (np.ndarray[floats]): precomputed likelihoods for each sample and copy number type.
+#                                         dim = nbOfRelevantRows * nbOfRelevantCols
+def observationCounts2Likelihoods(clusterID, samples, counts, clust2samps, exp_loc, exp_scale,
+                                  exonCN2Params, numCNs, numParamsCols):
+
+    # Convert the dictionary keys to a list and find the index of the clusterID
+    clusterIDs = list(clust2samps.keys())
+    clusterIndex = clusterIDs.index(clusterID)
+
+    # Get the samples belonging to the specified cluster
+    sampsIDs = clust2samps[clusterID]
+    sampsIndexes = np.where(np.isin(sampsIDs, samples))[0]
+
+    numSamps = len(sampsIndexes)
+
+    # Extract the relevant columns from exonCN2Params for the specified cluster
+    CN2paramsClust = exonCN2Params[:, clusterIndex * numParamsCols:(clusterIndex + 1) * numParamsCols]
+
+    # Create an array of column indices for each sample,
+    # np.tile: repeating the pattern [0, 1, 2, 3] numSamps times
+    relevantCols = np.tile(np.arange(numCNs), numSamps) + np.repeat(sampsIndexes, numCNs) * numCNs
+    # relevantCols = [ci + sampsIndex * numCNs for sampsIndex in sampsIndexes for ci in range(numCNs)]
+
+    # Find the row indices where the third column of CN2paramsCluster is equal to 4 ("Calls")
+    relevantRows = np.where(CN2paramsClust[:, 2] == 4)[0]
+
+    likelihoodArray = allocateLikelihoodsArray(numSamps, len(relevantRows), numCNs)
+
+    for rowIndex, exonIndex in enumerate(relevantRows):
+        FPMs = counts[exonIndex, sampsIndexes]
+        # Get the distribution parameters for this exon
+        CN_params = getDistributionParams(exp_loc, exp_scale, CN2paramsClust[exonIndex, 0], CN2paramsClust[exonIndex, 1])
+
+        for ci in range(numCNs):
+            distribution, loc, scale = CN_params[ci]
+
+            # Compute the likelihoods for the current CNStatus using vectorized operation
+            CNLikelihoods = distribution.pdf(FPMs, loc=loc, scale=scale)
+
+            # Compute the column indices using vectorized operations
+            colIndices = np.arange(ci, numSamps * numCNs, numCNs)
+
+            # Assign values to likelihoodArray using correct indexes
+            likelihoodArray[rowIndex, colIndices] = CNLikelihoods
+
+    return (relevantCols, relevantRows, likelihoodArray)
 
 
 ###############################################################################
 ############################ PRIVATE FUNCTIONS ################################
 ###############################################################################
-
-
+######################################
+# getDistributionParams
+# Get the parameters of the continuous distributions for each copy number type.
+#
+# Args:
+# - exp_loc [float]: Location parameter for the exponential distribution (CN0).
+# - exp_scale [float]: 1 / lambda parameter for the exponential distribution (CN0).
+# - gauss_loc [float]: Mean parameter for the Gaussian distribution (CN2).
+# - gauss_scale [float]: Standard deviation parameter for the Gaussian distribution (CN2).
+# Returns:
+# - CN_params (tupple): contains the distribution function, location parameter,
+#                       and scale parameter for a specific copy number type.
+def getDistributionParams(exp_loc, exp_scale, gauss_loc, gauss_scale):
+    CN_params = [
+        (scipy.stats.expon, exp_loc, exp_scale),  # CN0
+        (scipy.stats.norm, gauss_loc / 2, gauss_scale),  # CN1
+        (scipy.stats.norm, gauss_loc, gauss_scale),  # CN2
+        (scipy.stats.norm, 3 * (gauss_loc / 2), gauss_scale),  # CN3
+    ]
+    return CN_params
