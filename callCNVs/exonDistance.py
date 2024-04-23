48acb31b6f334f7ad7557f1ce3d70a988217c275
septiera <amandine.septier@univ-grenoble-alpes.fr>
2024-04-23 15:31:17 +0200

    callCNVs/exonDistance.py: Adjusted probability calculation to use power law instead of exponential function for probability adjustment. Simplified code accordingly.

--- callCNVs/exonDistance.py
+++ callCNVs/exonDistance.py
@@ -11,52 +11,38 @@ logger = logging.getLogger(__name__)
 ############################ PUBLIC FUNCTIONS #################################
 ###############################################################################
 ########################
-# initAdjustTransMatrix
+# adjustTransMatrix
 # When distance between exons increases, the correlation between CN states of
-# consecutive exons diminishes until at some point with simply expect the priors.
+# consecutive exons diminishes until at some point we simply expect the priors.
 # Formally we implement this as follows:
-# if transition probability to CN2 > priorCN2 leave untouched;
-# else:
-#   - transition to CN2 is inflated following an exponential law
-#   until reaching the prior at d = dmax,
-#   - transitions to CN0,CN1,CN3+ are reduced uniformly to compensate.
+#   - transition probabilities are smoothed from base probas to prior probas following
+#   a power law until reaching the prior at d = dmax,
 #
 # Args:
-# - transMatrix (np.array): Matrix of base transition probabilities between states.
+# - transMatrix (np.array): base transition probabilities between states.
+# - d [int]: distance between exons
 # - dmax (float): Distance where prior is reached. Default 18000bp from Freeman GenomeRes 2006.
 #
-# Returns:
-# - function: adjustTransMatrix(d)
-#    - Arg: d [int]: distance between exons
-#    - return: adjusted transition probability matrix (np.ndarray dim(5*5))
+# Returns: adjusted transition probability matrix (np.ndarray dim(5*5))
 # as defined above
-def initAdjustTransMatrix(transMatrix, dmax=18000):
+def adjustTransMatrix(transMatrix, d, dmax=18000):
+    # hardcoded power of the power law, values between 3-6 should be reasonable
+    N = 5
 
-    def adjustTransMatrix(d):
-        priorCN2 = transMatrix[0, 3]
-        numStatesWithVoid = transMatrix.shape[0]
-        newTrans = numpy.zeros_like(transMatrix)
+    numStatesWithVoid = transMatrix.shape[0]
+    newTrans = numpy.zeros_like(transMatrix)
 
-        if d >= dmax:
-            for prevState in range(numStatesWithVoid):
-                newTrans[prevState, :] = transMatrix[0, :].copy()
+    if d >= dmax:
+        for prevState in range(numStatesWithVoid):
+            newTrans[prevState, :] = transMatrix[0, :].copy()
 
-        else:
-            # copy prior
-            newTrans[0, :] = transMatrix[0, :].copy()
+    else:
+        # newTrans[p->i](x) = trans[p->i] + (prior[i] - trans[p->i]) * (x/d_max)^N
+        for state in range(1, numStatesWithVoid):
+            newTrans[:, state] = (transMatrix[:, state] +
+                                  (transMatrix[0, state] - transMatrix[:, state]) * (d / dmax) ** N)
 
-            for prevState in range(1, numStatesWithVoid):
-                probTransCN2 = transMatrix[prevState, 3]
-                if probTransCN2 > priorCN2:
-                    newTrans[prevState, :] = transMatrix[prevState, :].copy()
-                else:
-                    fmax = priorCN2 / probTransCN2
-                    f_d = fmax ** (d / dmax)
-                    g_d = (1 - probTransCN2 * f_d) / (1 - probTransCN2)
+        # squash first row with prior
+        newTrans[0, :] = transMatrix[0, :].copy()
 
-                    newTrans[prevState, :] = g_d * transMatrix[prevState, :].copy()
-                    newTrans[prevState, 3] = f_d * probTransCN2
-
-        return newTrans
-
-    return adjustTransMatrix
+    return newTrans
