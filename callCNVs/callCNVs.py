b589f1986667b2413829b180ca5a211a8bcc7f16
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2024-04-27 13:19:55 +0200

    adjustTransMatrix() moved to callCNVs and needs priors

--- callCNVs/callCNVs.py
+++ callCNVs/callCNVs.py
@@ -374,3 +374,38 @@ def buildCNVs(calledExons, path, bestPathProbas, CN2PathProbas, lastState, sampl
             firstExonInCurrentState = cei
 
     return(CNVs)
+
+
+######################################
+# adjustTransMatrix
+# When distance between exons increases, the correlation between CN states of
+# consecutive exons diminishes until at some point we simply expect the priors.
+# Formally we implement this as follows: transition probabilities are smoothed
+# from base probas to prior probas following a power law, until reaching the
+# prior at d = dmax.
+#
+# Args:
+# - transMatrix (ndarray dim 4*4): base transition probabilities between states
+# - priors (ndarray dim 4): prior probabilities for each state
+# - d [int]: distance between exons
+# - dmax (float): distance where prior is reached
+#
+# Returns: adjusted transition probability matrix (ndarray dim 4*4)
+def adjustTransMatrix(transMatrix, priors, d, dmax):
+    # hardcoded power of the power law, values between 3-6 should be reasonable
+    N = 5
+
+    newTrans = numpy.zeros_like(transMatrix)
+
+    if d >= dmax:
+        # use priors
+        for prevState in range(len(transMatrix)):
+            newTrans[prevState] = priors
+
+    else:
+        # newTrans[p->i](x) = trans[p->i] + (prior[i] - trans[p->i]) * (x/d_max)^N
+        for state in range(len(transMatrix)):
+            newTrans[:, state] = (transMatrix[:, state] +
+                                  (priors[state] - transMatrix[:, state]) * (d / dmax) ** N)
+
+    return newTrans
