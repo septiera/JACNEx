602a81dfdf001370083186bf6d03c256d25e6dab
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2024-05-14 04:32:28 +0200

    likelihoodsNTM.py: fitCN2andCalcLikelihoods() implemented

--- callCNVs/likelihoodsNTM.py
+++ callCNVs/likelihoodsNTM.py
@@ -1,6 +1,7 @@
 import logging
 import numpy
 import scipy.stats
+import statistics
 
 ####### JACNEx modules
 import callCNVs.robustGaussianFit
@@ -54,7 +55,7 @@ def fitCNO(intergenicFPMs):
 # - FPMs: numpy 2D-array of floats, FPMs[e,s] is the FPM-normalized count for exon
 #   e in sample s - the caller must know what samples and exons are present and in
 #   what order
-# - likelihoods:numpy 3D-array of floats (pre-allocated) of size
+# - likelihoods: numpy 3D-array of floats (pre-allocated) of size
 #   nbSamples (==nbColumns in FPMs) * nbExons (==nbRows in FPMs) * nbStates;
 #   likelihoods[s,e,cn] is the likehood of state cn for exon e in sample s
 #   (same s and e indexes as in FPMs)
@@ -72,22 +73,77 @@ def calcLikelihoodsCN0(FPMs, likelihoods, CN0scale):
 # for each exon (==row of FPMsOfCluster):
 # - fit a normal distribution to the dominant component of the FPMs (this is
 #   our model of CN2, we assume that most samples are CN2)
-# - if fitting fails one of the QC criteria, exon is NOCALL => set likelihoods to -1;
+# - if one of the fitCN2() QC criteria, exon is NOCALL => set likelihoods to -1;
 # - else:
-#   - rescale FPMS so CN2 mean == 1 (goal: have comparable likelihoods between exons)
+#   - rescale FPMS and CN2 model so new CN2 mean == 1 (idea: pdfs integrate to 1, so
+#     without this rescaling high-FPM exons have small likelihoods compared to
+#     low-FPM exons, resulting in smaller scores for CNVs that span them)
 #   - calculate likelihoods for CN1, CN2, CN3+
 #
 # Args:
-# - intergenicFPMs numpy 2D-array of floats, size=len(intergenics)] * len(samples),
-#   holding the FPM-normalized counts for intergenic pseudo-exons
+# - FPMsOfCluster: 2D-array of floats, FPMsOfCluster[e,s] is the FPM count for
+#   exon e in sample s, the samples must include those in FITWITH clusters (in
+#   addition to the samples in the current cluster of interest)
+# - samplesOfInterest: 1D-array of bools, size = number of samples in FPMsOfCluster,
+#   value==True if the sample is in the cluster of interest (vs being in a FITWITH cluster)
+# - likelihoods: numpy 3D-array of floats (pre-allocated) of size
+#   nbSamplesOfInterest * nbExons (==nbRows in FPMsOfCluster) * nbStates;
+#   likelihoods[s,e,cn] is the likehood of state cn for exon e in sample s
+# - isHaploid: bool, if True this cluster of samples is assumed to be haploid
+#   for all chromosomes where the exons are located (eg chrX and xhrY in men).
 #
-# Returns (CN0scale, fpmThreshold):
-# - CN0scale is the scale parameter of the fitted half-normal distribution
-# - fpmThreshold is the FPM threshold up to which data looks like it could very possibly
-#   have been produced by the CN0 model (set to fracPPF of the inverse CDF == quantile
-#   function). This will be used later for filtering NOCALL exons.
-def fitCN2andCalcLikelihoods(FPMsOfCluster):
-    return
+# Returns (CN2Means):
+# - CN2means: 1D-array of nbExons floats, CN2means[e] is the fitted mean of
+#   the CN2 model (before rescaling) of exon e for the cluster, or -1 if exon
+#   is NOCALL
+def fitCN2andCalcLikelihoods(FPMsOfCluster, samplesOfInterest, likelihoods, fpmThreshold, isHaploid):
+    # sanity
+    nbExons = FPMsOfCluster.shape[0]
+    nbSamplesTotal = FPMsOfCluster.shape[1]
+    nbSOIs = samplesOfInterest.sum()
+    if ((nbExons != likelihoods.shape[1]) or
+        (nbSamplesTotal != samplesOfInterest.shape[0]) or
+        (nbSOIs != likelihoods.shape[0])):
+        logger.error("sanity check failed in fitCN2andCalcLikelihoods(), impossible!")
+        raise Exception("fitCN2andCalcLikelihoods sanity check failed")
+
+    CN2means = numpy.full(nbExons, fill_value=-1, dtype=numpy.float64)
+
+    if isHaploid:
+        # set all likelihoods of CN1 to zero
+        likelihoods[:, :, 1] = 0
+
+    for ei in range(nbExons):
+        (cn2Mu, cn2Sigma) = fitCN2(FPMsOfCluster[ei, :], fpmThreshold)
+        if cn2Mu < 0:
+            # exon is NOCALL for the whole cluster, squash likelihoods to -1
+            likelihoods[:, ei, :] = -1.0
+            # we could also calculate statistics on which QC criteria failed in
+            # fitCN2(), based on the cn2Mu values, but no time now
+            continue
+
+        # save CN2 mean before rescaling
+        CN2means[ei] = cn2Mu
+        # now rescale CN2 sigma and FPMs of SOIs
+        cn2SigmaRescaled = cn2Sigma / cn2Mu
+        fpmsRescaled = FPMsOfCluster[ei, samplesOfInterest] / cn2Mu
+
+        # CN1: shift the CN2 Gaussian so mean==0.5 (a single copy rather than 2)
+        cn1Dist = statistics.NormalDist(mu=0.5, sigma=cn2SigmaRescaled)
+
+        # CN2 model: the fitted Gaussian
+        cn2Dist = statistics.NormalDist(mu=1.0, sigma=cn2SigmaRescaled)
+
+        # CN3 model, as defined in cn3Dist()
+        cn3Dist = cn3Distrib(1.0, cn2SigmaRescaled)
+
+        for si in range(nbSOIs):
+            if not isHaploid:
+                likelihoods[si, ei, 1] = cn1Dist.pdf(fpmsRescaled[si])
+            likelihoods[si, ei, 2] = cn2Dist.pdf(fpmsRescaled[si])
+            likelihoods[si, ei, 3] = cn3Dist.pdf(fpmsRescaled[si])
+
+    return(CN2means)
 
 
 ###############################################################################
@@ -104,18 +160,25 @@ def fitCN2andCalcLikelihoods(FPMsOfCluster):
 # - CN2 model isn't supported by 50% or more of the samples
 # - CN2 Gaussian can't be clearly distinguished from CN0 model
 #
-# If one of the QC criteria fails, raise an exception;
-# otherwise return (mu,sigma) == mean and stdev of the CN2 model
+# If all QC criteria pass, return (mu,sigma) == mean and stdev of the CN2 model;
+# otherwise return (E, 0) where E<0 depends on the first criteria that failed:
+# E==-1 if exon isn't captured
+# E==-2 if robustGaussianFit failed
+# E==-3 if low support for CN2 model
+# E==-4 if CN2 is too close to CN0
 def fitCN2(FPMsOfExon, fpmThreshold):
     if numpy.median(FPMsOfExon) <= fpmThreshold:
-        raise Exception("uncaptured exon")
+        # uncaptured exon
+        return(-1, 0)
 
     try:
         (mu, sigma) = callCNVs.robustGaussianFit.robustGaussianFit(FPMsOfExon)
     except Exception as e:
         if str(e) != "cannot fit":
             logger.warning("robustGaussianFit failed unexpectedly: %s", repr(e))
-        raise
+            raise
+        else:
+            return(-2, 0)
 
     # require at least minSamps samples within sdLim sigmas of mu
     minSamps = len(FPMsOfExon) * 0.5
@@ -123,11 +186,38 @@ def fitCN2(FPMsOfExon, fpmThreshold):
     samplesUnderCN2 = numpy.sum(numpy.logical_and(FPMsOfExon - mu - sdLim * sigma < 0,
                                                   FPMsOfExon - mu + sdLim * sigma > 0))
     if samplesUnderCN2 < minSamps:
-        raise Exception("low support for CN2")
+        # low support for CN2
+        return(-3, 0)
 
     # require CN2 to be at least minZscore sigmas from fpmThreshold
     minZscore = 3
     if (mu - minZscore * sigma) <= fpmThreshold:
-        raise Exception("CN2 too close to CN0")
+        # CN2 too close to CN0
+        return(-4, 0)
 
     return(mu, sigma)
+
+
+############################################
+# cn3Distrib:
+# build a statistical model of CN3+, based on the CN2 mu and sigma.
+# CN3+ is modeled as a Gamma distribution (as implemented by AS) for now, although
+# it's empirical and I'm not sure it behaves as expected after rescaling?
+#
+# Args: (mu, sigma) of the CN2 model
+#
+# Return an object with a pdf() method - currently a "frozen" scipy distribution, but
+# it could be some other object (eg statistics.NormalDist).
+def cn3Distrib(mu, sigma):
+    # The 'shape' parameter is hard-coded here, based on empirical testing.
+    # A higher 'shape' concentrates the distribution around the mean and reduces the spread
+    shape = 6
+    # The 'loc' parameter shifts the gamma distribution to avoid significant overlap
+    # with the Gaussian CN2 distribution
+    loc = mu + sigma
+    # The 'scale' parameter: the logarithm helps to reduce the scale of values, making the model more
+    # adaptable and stable, especially if loc varies over a wide range.
+    scale = numpy.log10(loc + 1)
+
+    cn3dist = scipy.stats.gamma(shape, loc=loc, scale=scale)
+    return(cn3dist)
